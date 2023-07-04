7704a30bb5cf3c87042e3b49d4427bd10f7e6a63
steven nevins <steven.nevins.js@gmail.com>
2023-07-04 16:12:53 -0400

    migrate some unit tests

---
+++ test/foundry/MintPassGroup.t.sol
@@ -0,0 +1,106 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {MintPassGroup} from "contracts/mint-pass-group/MintPassGroup.sol";
+
+contract MintPassGroupTest is Test {
+    address public admin =
+        vm.addr(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80);
+    address public fxHashAdmin =
+        vm.addr(0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d);
+    address public user1 =
+        vm.addr(0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a);
+    address public user2 =
+        vm.addr(0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6);
+
+    /// project details
+    string public token = "TOKEN1";
+    uint256 public project = 1;
+    address public addr = user1;
+
+    MintPassGroup public mintPassGroup;
+
+    function setUp() public virtual {
+        mintPassGroup = new MintPassGroup(
+            10, /* maxPerToken */
+            5, /* maxPerTokenPerProject */
+            fxHashAdmin, /* signer (authorized caller)*/
+            new address[](0) /* bypass array */
+        );
+    }
+}
+
+contract GetProjectHash is MintPassGroupTest {
+    function test_True() public {
+        assertTrue(true);
+    }
+}
+
+contract IsValidPass is MintPassGroupTest {
+    function test_True() public {
+        assertTrue(true);
+    }
+}
+
+contract ConsumePass is MintPassGroupTest {
+    function test_True() public {
+        assertTrue(true);
+    }
+}
+
+contract SetConstraints is MintPassGroupTest {
+    function test_RevertsWhenNotAuthorized() public {
+        vm.expectRevert();
+        mintPassGroup.setConstraints(1, 1);
+        /// check constraints
+    }
+
+    function test_setConstraints() public {
+        vm.prank(fxHashAdmin);
+        mintPassGroup.setConstraints(1, 1);
+        /// check constraints
+    }
+}
+
+contract SetBypass is MintPassGroupTest {
+    function test_RevertsWhenNotAuthorized() public {
+        address[] memory bypassArray = new address[](1);
+        bypassArray[0] = fxHashAdmin;
+        vm.expectRevert();
+        mintPassGroup.setBypass(bypassArray);
+    }
+
+    function test_setBypass() public {
+        address[] memory bypassArray = new address[](1);
+        bypassArray[0] = fxHashAdmin;
+        vm.prank(fxHashAdmin);
+        mintPassGroup.setBypass(bypassArray);
+        assertTrue(mintPassGroup.getBypass().length == 1, "Bypass array not set");
+        /// check contents
+    }
+
+    function test_whenExists() public {
+        address[] memory bypassArray = new address[](1);
+        bypassArray[0] = fxHashAdmin;
+        vm.prank(fxHashAdmin);
+        mintPassGroup.setBypass(bypassArray);
+        /// probably want to revert if duplicating
+        vm.prank(fxHashAdmin);
+        mintPassGroup.setBypass(bypassArray);
+    }
+}
+
+contract GetBypass is MintPassGroupTest {
+    function test_whenEmpty() public {
+        assertTrue(mintPassGroup.getBypass().length == 0, "Bypass array not 0");
+    }
+
+    function test_whenSet() public {
+        address[] memory bypassArray = new address[](1);
+        bypassArray[0] = fxHashAdmin;
+        vm.prank(fxHashAdmin);
+        mintPassGroup.setBypass(bypassArray);
+        assertTrue(mintPassGroup.getBypass().length == 1, "Bypass array not set");
+    }
+}
