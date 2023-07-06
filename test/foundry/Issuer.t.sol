b73b0511504d9e598f0adb0f75b8351815e565f2
steven nevins <steven.nevins.js@gmail.com>
2023-07-06 12:44:19 -0400

    deployment setup

--- test/foundry/Issuer.t.sol
+++ test/foundry/Issuer.t.sol
@@ -6,32 +6,105 @@ import {MintPassGroup} from "contracts/mint-pass-group/MintPassGroup.sol";
 import {ContentStore} from "contracts/scripty/dependencies/ethfs/ContentStore.sol";
 import {ScriptyStorage} from "contracts/scripty/ScriptyStorage.sol";
 import {ScriptyBuilder} from "contracts/scripty/ScriptyBuilder.sol";
+import {ReserveWhitelist} from "contracts/reserve/ReserveWhitelist.sol";
+import {ReserveMintPass} from "contracts/reserve/ReserveMintPass.sol";
+import {ModerationToken} from "contracts/moderation/ModerationToken.sol";
+import {ModerationUser} from "contracts/moderation/ModerationUser.sol";
+import {ModerationTeam} from "contracts/moderation/ModerationTeam.sol";
+import {UserActions} from "contracts/issuer/UserActions.sol";
+import {AllowMint} from "contracts/allow-mint/AllowMint.sol";
+import {AllowMintIssuer} from "contracts/allow-mint/AllowMintIssuer.sol";
+import {PricingDutchAuction} from "contracts/pricing/PricingDutchAuction.sol";
+import {PricingFixed} from "contracts/pricing/PricingFixed.sol";
+import {Randomizer} from "contracts/randomizer/Randomizer.sol";
+import {ReserveManager} from "contracts/issuer/ReserveManager.sol";
+import {PricingManager} from "contracts/issuer/PricingManager.sol";
+import {Issuer, IIssuer} from "contracts/issuer/Issuer.sol";
+import {MintTicket} from "contracts/mint-ticket/MintTicket.sol";
+import {GenTk} from "contracts/gentk/GenTk.sol";
+import {OnChainTokenMetadataManager} from "contracts/issuer/OnchainTokenMetadataManager.sol";
 
 contract IssuerTest is Test {
-    bytes32 public adminPk =
-        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-    bytes32 public fxHashAdminPk =
-        0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d;
-    bytes32 public user1Pk =
-        0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a;
-    bytes32 public user2Pk =
-        0x7c852118294e51e653712a81e05800f419141751be58f605c371e15141b007a6;
-
-    address public admin = vm.addr(uint256(adminPk));
-    address public fxHashAdmin = vm.addr(uint256(fxHashAdminPk));
-    address public user1 = vm.addr(uint256(user1Pk));
-    address public user2 = vm.addr(uint256(user2Pk));
+    address public admin = address(1);
+    address public receiver = address(2);
+    address public signer = address(3);
+    address public treasury = address(4);
+    address public addr1 = address(5);
+    address public addr2 = address(6);
+    address public addr3 = address(7);
 
     uint256[2] public authorizations = [10, 20];
     /// scripty
     ContentStore public contentStore;
     ScriptyStorage public scriptyStorage;
     ScriptyBuilder public scriptyBuilder;
+    ReserveWhitelist public reserveWhitelist;
+    ReserveMintPass public reserveMintPass;
+    MintPassGroup public mintPassGroup;
+    ModerationToken public moderationToken;
+    ModerationTeam public moderationTeam;
+    ModerationUser public moderationUser;
+    UserActions public userActions;
+    AllowMint public allowMint;
+    PricingDutchAuction public pricingDA;
+    PricingFixed public pricingFixed;
+    Randomizer public randomizer;
+    PricingManager public pricingManager;
+    ReserveManager public reserveManager;
+    AllowMintIssuer public allowMintIssuer;
+    Issuer public issuer;
+    MintTicket public mintTicket;
+    GenTk public genTk;
+    OnChainTokenMetadataManager public onchainMetadataManager;
 
     function setUp() public virtual {
+        /// move this stuff to Script and import here
         contentStore = new ContentStore();
         scriptyStorage = new ScriptyStorage(address(contentStore));
         scriptyBuilder = new ScriptyBuilder();
+        reserveWhitelist = new ReserveWhitelist();
+        reserveMintPass = new ReserveMintPass();
+        mintPassGroup = new MintPassGroup(
+            10, /* maxPerToken */
+            5, /* maxPerTokenPerProject */
+            admin, /* signer (authorized caller)*/
+            new address[](0) /* bypass array */
+        );
+        moderationToken = new ModerationToken(admin);
+        moderationTeam = new ModerationTeam(admin);
+        moderationUser = new ModerationUser(admin);
+        userActions = new UserActions(admin);
+        allowMint = new AllowMint(
+            admin,
+            address(moderationToken),
+            address(userActions)
+        );
+        pricingDA = new PricingDutchAuction();
+        pricingFixed = new PricingFixed();
+        randomizer = new Randomizer(keccak256("seed"), keccak256("salt"));
+        pricingManager = new PricingManager(admin);
+        reserveManager = new ReserveManager(admin);
+        allowMintIssuer = new AllowMintIssuer(
+            admin,
+            address(moderationUser),
+            address(userActions)
+        );
+        issuer = new Issuer(IIssuer.Config(2500, 1000, 1000, ""), admin);
+        mintTicket = new MintTicket(
+            admin,
+            address(issuer),
+            address(randomizer)
+        );
+        onchainMetadataManager = new OnChainTokenMetadataManager(
+            address(scriptyBuilder)
+        );
+        genTk = new GenTk(
+            admin,
+            signer,
+            treasury,
+            address(issuer),
+            address(onchainMetadataManager)
+        );
     }
 }
 
