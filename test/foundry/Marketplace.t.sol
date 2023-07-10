f0f4573a7ad4a5a6f2ca8aa0cf867fd5cb1b703e
steven nevins <steven.nevins.js@gmail.com>
2023-07-10 08:03:15 -0400

    add marketplace

---
+++ test/foundry/Marketplace.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract MarketPlaceTest is Test {
+    address public admin = address(1);
+    address public fxHashAdmin = address(2);
+    address public addr1 = address(3);
+
+    function setUp() public virtual {}
+}
