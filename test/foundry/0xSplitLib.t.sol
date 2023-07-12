53ec64b549803d68f384527396a9b7e9d3565973
steven nevins <steven.nevins.js@gmail.com>
2023-07-12 17:36:19 -0400

    add helper lib for working with splits

---
+++ test/foundry/0xSplitLib.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+
+contract FooTest is Test {
+    function test_True() public {
+        assertTrue(true);
+    }
+}
