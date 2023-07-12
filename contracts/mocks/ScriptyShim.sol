61d8ac2b743b739f13ec8efddb4f72a0fa31cb45
steven <12021290+stevennevins@users.noreply.github.com>
2023-07-12 09:24:58 -0400

    remove scripty dir and use installed dep (#29)

---
+++ contracts/mocks/ScriptyShim.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {ContentStore} from "scripty.sol/contracts/scripty/dependencies/ethfs/ContentStore.sol";
+import {ScriptyBuilder} from "scripty.sol/contracts/scripty/ScriptyBuilder.sol";
+import {ScriptyStorage} from "scripty.sol/contracts/scripty/ScriptyStorage.sol";
+
+contract Shim {}
