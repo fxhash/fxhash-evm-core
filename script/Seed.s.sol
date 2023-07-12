1364511c8003a330fbd20883826335f5fb4b95a4
Florian PAUTOT <florian.pautot@gmail.com>
2023-07-12 14:00:37 +0200

    add get issuer function

--- script/Seed.s.sol
+++ script/Seed.s.sol
@@ -15,11 +15,28 @@ import {LibCodex} from "contracts/libs/LibCodex.sol";
 import {WrappedScriptRequest} from "scripty.sol/contracts/scripty/IScriptyBuilder.sol";
 
 contract Seed is Script {
-
-    enum ReserveOptions { None, Whitelist, MintPass, WhitelistAndMintPass }
-    enum PricingOptions { Fixed, DutchAuction }
-    enum OpenEditions { Enabled, Disabled }
-    enum MintTicket { Enabled, Disabled }
+    enum ReserveOptions {
+        None,
+        Whitelist,
+        MintPass,
+        WhitelistAndMintPass
+    }
+    enum PricingOptions {
+        Fixed,
+        DutchAuction
+    }
+    enum OpenEditionsOptions {
+        Enabled,
+        Disabled
+    }
+    enum MintTicketOptions {
+        Enabled,
+        Disabled
+    }
+    enum OnChainOptions {
+        Enabled,
+        Disabled
+    }
 
     Deploy public deploy;
 
@@ -44,41 +61,99 @@ contract Seed is Script {
     //  - Pricing (Fixed price/Dutch Auction)
     //  - On chain / off chain
 
-    function getEmptyMintTicketParam() public pure returns (IIssuer.MintTicketSettings memory){
-        return IIssuer.MintTicketSettings({
-            gracingPeriod: 0,
-            metadata: ""
-        });
+    function getIssuer(
+        ReserveOptions reserveOption,
+        PricingOptions pricingOption,
+        OpenEditionsOptions openEditionOption,
+        MintTicketOptions mintTicketOptions,
+        OnChainOptions onChainOptions
+    ) public view returns (IIssuer.MintIssuerInput memory){
+        LibReserve.ReserveData[] memory reserveData;
+        LibPricing.PricingData memory pricingData;
+        LibIssuer.OpenEditions memory openEditionsData;
+        IIssuer.MintTicketSettings memory mintTicketData;
+        WrappedScriptRequest[] memory onChainData;
+
+        if (reserveOption == ReserveOptions.None) {
+            reserveData = getEmptyReserveParam();
+        } else if (reserveOption == ReserveOptions.Whitelist) {
+            reserveData = getWhitelistParam();
+        } else if (reserveOption == ReserveOptions.MintPass) {
+            reserveData = getMintPassParam();
+        } else if (reserveOption == ReserveOptions.WhitelistAndMintPass) {
+            reserveData = getWhitelistAndMintPassParam();
+        }
+
+        if (pricingOption == PricingOptions.Fixed) {
+            pricingData = getFixedPriceParam();
+        } else {
+            pricingData = getDutchAuctionParam();
+        }
+
+        if (openEditionOption == OpenEditionsOptions.Enabled) {
+            openEditionsData = getOpenEditionParam();
+        } else {
+            openEditionsData = getEmptyOpenEditionParam();
+        }
+
+        if (mintTicketOptions == MintTicketOptions.Enabled) {
+            mintTicketData = getMintTicketParam();
+        } else {
+            mintTicketData = getEmptyMintTicketParam();
+        }
+
+        if (onChainOptions == OnChainOptions.Enabled) {
+            onChainData = getOnChainScripts();
+        } else {
+            onChainData = getEmptyOnChainScripts();
+        }
+        return
+            IIssuer.MintIssuerInput({
+                codex: LibCodex.CodexInput({
+                    inputType: 0,
+                    value: bytes(""),
+                    codexId: 0,
+                    issuer: address(deploy.issuer())
+                }),
+                metadata: bytes(""),
+                inputBytesSize: 0,
+                amount: 10,
+                openEditions: openEditionsData,
+                mintTicketSettings: mintTicketData,
+                reserves: reserveData,
+                pricing: pricingData,
+                primarySplit: getSplit(),
+                royaltiesSplit: getSplit(),
+                enabled: true,
+                tags: new uint256[](0),
+                onChainScripts: onChainData
+            });
     }
 
-    function getMintTicketParam() public pure returns (IIssuer.MintTicketSettings memory){
-        return IIssuer.MintTicketSettings({
-            gracingPeriod: 1000,
-            metadata: "ipfs://1234"
-        });
+    function getEmptyMintTicketParam() public pure returns (IIssuer.MintTicketSettings memory) {
+        return IIssuer.MintTicketSettings({gracingPeriod: 0, metadata: ""});
     }
 
-    function getOpenEditionParam() public view returns(LibIssuer.OpenEditions memory){
-        return LibIssuer.OpenEditions({
-            closingTime: block.timestamp + 1000,
-            extra: bytes("")
-        });
+    function getMintTicketParam() public pure returns (IIssuer.MintTicketSettings memory) {
+        return IIssuer.MintTicketSettings({gracingPeriod: 1000, metadata: "ipfs://1234"});
     }
 
-    function getEmptyOpenEditionParam() public pure returns(LibIssuer.OpenEditions memory) {
-        return LibIssuer.OpenEditions({
-            closingTime: 0,
-            extra: bytes("")
-        });
+    function getOpenEditionParam() public view returns (LibIssuer.OpenEditions memory) {
+        return LibIssuer.OpenEditions({closingTime: block.timestamp + 1000, extra: bytes("")});
     }
 
-    function getEmptyReserveParam() public pure returns (LibReserve.ReserveData[] memory){
+    function getEmptyOpenEditionParam() public pure returns (LibIssuer.OpenEditions memory) {
+        return LibIssuer.OpenEditions({closingTime: 0, extra: bytes("")});
+    }
+
+    function getEmptyReserveParam() public pure returns (LibReserve.ReserveData[] memory) {
         return new LibReserve.ReserveData[](0);
     }
 
-    function getWhitelistParam() public view returns (LibReserve.ReserveData[] memory){
+    function getWhitelistParam() public view returns (LibReserve.ReserveData[] memory) {
         LibReserve.ReserveData[] memory reserves = new LibReserve.ReserveData[](1);
-        ReserveWhitelist.WhitelistEntry[] memory whitelistEntries = new ReserveWhitelist.WhitelistEntry[](3);
+        ReserveWhitelist.WhitelistEntry[]
+            memory whitelistEntries = new ReserveWhitelist.WhitelistEntry[](3);
 
         whitelistEntries[0] = ReserveWhitelist.WhitelistEntry({
             whitelisted: deploy.bob(),
@@ -101,7 +176,7 @@ contract Seed is Script {
         return reserves;
     }
 
-    function getMintPassParam() public view returns (LibReserve.ReserveData[] memory){
+    function getMintPassParam() public view returns (LibReserve.ReserveData[] memory) {
         LibReserve.ReserveData[] memory reserves = new LibReserve.ReserveData[](1);
         reserves[0] = LibReserve.ReserveData({
             methodId: 2,
@@ -111,9 +186,10 @@ contract Seed is Script {
         return reserves;
     }
 
-    function getWhitelistAndMintPassParam() public view returns (LibReserve.ReserveData[] memory){
+    function getWhitelistAndMintPassParam() public view returns (LibReserve.ReserveData[] memory) {
         LibReserve.ReserveData[] memory reserves = new LibReserve.ReserveData[](2);
-        ReserveWhitelist.WhitelistEntry[] memory whitelistEntries = new ReserveWhitelist.WhitelistEntry[](3);
+        ReserveWhitelist.WhitelistEntry[]
+            memory whitelistEntries = new ReserveWhitelist.WhitelistEntry[](3);
 
         whitelistEntries[0] = ReserveWhitelist.WhitelistEntry({
             whitelisted: deploy.bob(),
@@ -141,42 +217,43 @@ contract Seed is Script {
         return reserves;
     }
 
-    function getFixedPriceParam() public view returns (LibPricing.PricingData memory){
-        return LibPricing.PricingData({
-            pricingId: 1,
-            details: abi.encode(PricingFixed.PriceDetails({
-                price: 1000,
-                opensAt: block.timestamp
-            })),
-            lockForReserves: false
-        });
+    function getFixedPriceParam() public view returns (LibPricing.PricingData memory) {
+        return
+            LibPricing.PricingData({
+                pricingId: 1,
+                details: abi.encode(
+                    PricingFixed.PriceDetails({price: 1000, opensAt: block.timestamp})
+                ),
+                lockForReserves: false
+            });
     }
 
-    function getDutchAuctionParam() public view returns (LibPricing.PricingData memory){
+    function getDutchAuctionParam() public view returns (LibPricing.PricingData memory) {
         uint256[] memory levels = new uint256[](4);
         levels[0] = 1000;
         levels[1] = 500;
         levels[2] = 400;
         levels[3] = 300;
-        return LibPricing.PricingData({
-            pricingId: 1,
-            details: abi.encode(
-                PricingDutchAuction.PriceDetails({
-                    opensAt: block.timestamp,
-                    decrementDuration: 600,
-                    lockedPrice: 0,
-                    levels: levels
-                })
-            ),
-            lockForReserves: false
-        });
+        return
+            LibPricing.PricingData({
+                pricingId: 1,
+                details: abi.encode(
+                    PricingDutchAuction.PriceDetails({
+                        opensAt: block.timestamp,
+                        decrementDuration: 600,
+                        lockedPrice: 0,
+                        levels: levels
+                    })
+                ),
+                lockForReserves: false
+            });
     }
 
-    function getEmptyOnChainScripts() public pure returns (WrappedScriptRequest[] memory){
+    function getEmptyOnChainScripts() public pure returns (WrappedScriptRequest[] memory) {
         return new WrappedScriptRequest[](0);
     }
 
-    function getOnChainScripts() public view returns (WrappedScriptRequest[] memory){
+    function getOnChainScripts() public view returns (WrappedScriptRequest[] memory) {
         WrappedScriptRequest[] memory scriptRequests = new WrappedScriptRequest[](4);
         bytes memory emptyBytes = "";
         scriptRequests[0] = WrappedScriptRequest({
@@ -222,10 +299,7 @@ contract Seed is Script {
         return scriptRequests;
     }
 
-    function getSplit() public view returns (LibRoyalty.RoyaltyData memory){
-        return LibRoyalty.RoyaltyData({
-            percent: 1000,
-            receiver: deploy.alice()
-        });
+    function getSplit() public view returns (LibRoyalty.RoyaltyData memory) {
+        return LibRoyalty.RoyaltyData({percent: 1000, receiver: deploy.alice()});
     }
 }
