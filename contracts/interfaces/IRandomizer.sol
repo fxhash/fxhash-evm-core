688ab75f7c64155d81dcf73447e8e0ad07ba2d13
Sahil Mehta <samehta91@gmail.com>
2023-07-15 19:12:47 -0700

    Refactor and clean up randomizer

--- contracts/interfaces/IRandomizer.sol
+++ contracts/interfaces/IRandomizer.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
+/// @title IRandomizer
+/// @notice Generates and reveals randomizer seeds
 interface IRandomizer {
-    function generate(uint256 token_id) external;
+    struct TokenKey {
+        address issuer;
+        uint256 tokenId;
+    }
+
+    struct Seed {
+        bytes32 chainSeed;
+        uint256 serialId;
+        bytes32 revealed;
+    }
+
+    struct Commitment {
+        bytes32 seed;
+        bytes32 salt;
+    }
+
+    error AlreadySeeded();
+    error NoReq();
+    error OOR();
+
+    event RandomizerGenerate(uint256 tokenId, Seed seed);
+    event RandomizerReveal(uint256 tokenId, bytes32 seed);
+
+    function commit(bytes32 _seed, bytes32 _salt) external;
+
+    function generate(uint256 _tokenId) external;
+
+    function reveal(TokenKey[] memory _tokenList, bytes32 _seed) external;
+
+    function getTokenKey(address _issuer, uint256 _tokenId) external pure returns (bytes32);
 }
