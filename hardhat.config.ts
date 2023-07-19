4c6cda31d277f648ac07bc0b1acc18f2d6af6a88
Florian PAUTOT <florian.pautot@gmail.com>
2023-07-19 17:54:15 +0200

    remove hardcoded mnemonic and loadconfig from env file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,13 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-gas-reporter";
 import "hardhat-contract-sizer";
+import { config as dotenvConfig } from "dotenv";
+
+dotenvConfig();
 
 const config: HardhatUserConfig = {
   // Add this optional config to be able to run Forge scripts with hardhat local node
   networks: {
     hardhat: {
       accounts: {
-        mnemonic: "since swing fatigue addict shallow select derive pepper acoustic midnight code vague",
+        mnemonic: process.env.MNEMONIC,
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
         count: 10,
