01079818e292156b091905f89504390d483891fc
Florian PAUTOT <florian.pautot@gmail.com>
2023-07-20 22:30:13 +0200

    make factory use clones for issuer and gentk

--- contracts/factories/Factory.sol
+++ contracts/factories/Factory.sol
@@ -7,12 +7,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Factory is IFactory, Ownable {
     address internal fxhashFactory;
+    address internal implementation;
 
-    constructor(address _fxhashFactory) {
+    constructor(address _fxhashFactory, address _implementation) {
         fxhashFactory = _fxhashFactory;
+    implementation = _implementation;
     }
 
     function setFxHashFactory(address _fxhashFactory) external onlyOwner {
         fxhashFactory = _fxhashFactory;
+    }
+
+    function setImplementation(address _implementation) external onlyOwner {
+        implementation = _implementation;
     }
 }
