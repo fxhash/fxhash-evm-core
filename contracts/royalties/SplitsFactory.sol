199d2cc21cd19da779e00da0805769e2d66298c9
steven nevins <steven.nevins.js@gmail.com>
2023-07-20 11:16:24 -0400

    add natspec

--- contracts/royalties/SplitsFactory.sol
+++ contracts/royalties/SplitsFactory.sol
@@ -5,6 +5,14 @@ import {ISplitsMain} from "contracts/interfaces/ISplitsMain.sol";
 import {Lib0xSplits} from "contracts/libs/Lib0xSplits.sol";
 
 contract SplitsFactory {
+    /**
+     * @dev Emitted to indicate a split was created or where it will be deployed to.
+     * @param split The address the split contract will be deployed to.
+     * @param accounts The array of addresses that participate in the split.
+     * @param allocations The array of allocations for each account.
+     * @param controller The address of the controller contract. (address(0) for immutable splits)
+     * @param distributorFee The distributor fee percentage. (Currently not used)
+     */
     event SplitsInfo(
         address indexed split,
         address[] accounts,
@@ -13,11 +21,21 @@ contract SplitsFactory {
         uint32 distributorFee
     );
 
+    /**
+     * @dev Emits an event for the deterministic deployment address of a split.
+     * @param accounts The array of addresses that participate in the split.
+     * @param allocations The array of allocations for each account.
+     */
     function createVirtualSplit(address[] memory accounts, uint32[] memory allocations) external {
         address split = Lib0xSplits.predictDeterministicAddress(accounts, allocations);
         if (split.code.length == 0) emit SplitsInfo(split, accounts, allocations, address(0), 0);
     }
 
+    /**
+     * @dev Creates a split wallet
+     * @param accounts The array of addresses that participate in the split.
+     * @param allocations The array of allocations for each account.
+     */
     function createSplit(address[] memory accounts, uint32[] memory allocations) external {
         address split = ISplitsMain(Lib0xSplits.SPLITS_MAIN).createSplit(
             accounts,
