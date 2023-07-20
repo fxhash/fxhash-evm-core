456d11fc4635a3d5aa0e9b3b057d4918f91146e4
Florian PAUTOT <florian.pautot@gmail.com>
2023-07-20 14:26:09 +0200

    impl Factory for gentk and issuer

---
+++ contracts/factories/FxHashFactory.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {GenTk} from "contracts/gentk/GenTk.sol";
+import {Issuer} from "contracts/issuer/Issuer.sol";
+
+contract FxHashFactory {
+    event IssuerCreated(address indexed _owner, address _configManager, address indexed issuer);
+    event GenTkCreated(
+        address indexed _owner,
+        address indexed _issuer,
+        address _configManager,
+        address indexed genTk
+    );
+
+    constructor () {}
+
+    function createGenTk(
+        address _owner,
+        address _issuer,
+        address _configManager
+    ) private returns (address) {
+        require(_owner != address(0), "FxHashFactory: Invalid owner address");
+        require(_issuer != address(0), "FxHashFactory: Invalid issuer address");
+        require(_configManager != address(0), "FxHashFactory: Invalid configManager address");
+
+        GenTk newGenTk = new GenTk(_owner, _issuer, _configManager);
+        emit GenTkCreated(_owner, _issuer, _configManager, address(newGenTk));
+
+        return address(newGenTk);
+    }
+
+    function createIssuer(address _owner, address _configManager) private returns (address) {
+        require(_owner != address(0), "FxHashFactory: Invalid owner address");
+        require(_configManager != address(0), "FxHashFactory: Invalid configManager address");
+
+        Issuer newIssuer = new Issuer(_owner, _configManager);
+        emit IssuerCreated(_owner, _configManager, address(newIssuer));
+
+        return address(newIssuer);
+    }
+
+    function createProject(address _owner, address _configManager) external returns (address, address){
+        address issuer = createIssuer(_owner, _configManager);
+        address gentk = createGenTk(_owner, issuer, _configManager);
+        return (issuer, gentk);
+    }
+}
---
+++ test/foundry/FxHashFactory.t.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.18;
+
+import "forge-std/Test.sol";
+import {FxHashFactory} from "contracts/factories/FxHashFactory.sol";
+import {Deploy} from "script/Deploy.s.sol";
+
+contract FxHashFactoryTest is Test, Deploy {
+    FxHashFactory public factory;
+
+    function setUp() public virtual override {
+        factory = new FxHashFactory();
+    }
+}
+
+contract CreateProject is FxHashFactoryTest {
+    function setUp() public virtual override {
+        super.setUp();
+        createAccounts();
+        Deploy.run();
+    }
+
+    function test_createProject() public {
+        //vm.expectEmit(address(factory)); --> does not work for some reason
+        (address issuer, address gentk) = factory.createProject(alice, address(configurationManager));
+        assertNotEq(issuer, address(0));
+        assertNotEq(gentk, address(0));
+    }
+}
