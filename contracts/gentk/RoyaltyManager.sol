0f09bd2a39eb2d45854618af90a423b2c2205351
steven nevins <steven.nevins.js@gmail.com>
2023-07-27 07:44:50 -0400

    add natspec

--- contracts/gentk/RoyaltyManager.sol
+++ contracts/gentk/RoyaltyManager.sol
@@ -4,10 +4,13 @@ pragma solidity ^0.8.17;
 import {IRoyaltyManager} from "contracts/interfaces/IRoyaltyManager.sol";
 import {IERC2981Upgradeable} from "@openzeppelin/contracts-upgradeable/interfaces/IERC2981Upgradeable.sol";
 
+/// @title RoyaltyManager
+/// @notice A contract for managing royalties
 abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
     /// @notice A struct containing basisPoints and receiver address for a royalty
     RoyaltyInfo[] public royalties;
 
+    /// @dev Mapping of token IDs to token-specific royalties
     mapping(uint256 => RoyaltyInfo[]) public royaltyTokenInfo;
 
     /// @notice Emitted when the royalties for a set of receivers have been updated.
@@ -15,6 +18,10 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
     /// @param basisPoints the basis points to calculate royalty payments(1/100th of a percent) for
     /// each receiver.
     event TokenRoyaltiesUpdated(address payable[] receivers, uint96[] basisPoints);
+    /// @dev Emitted when the royalties for a specific token ID have been updated.
+    /// @param tokenId The token ID for which the royalties have been updated.
+    /// @param receivers The addresses that will receive royalties.
+    /// @param basisPoint The basis points to calculate royalty payments (1/100th of a percent) for each receiver.
     event TokenIdRoyaltiesUpdated(
         uint256 indexed tokenId,
         address payable[] receivers,
@@ -22,7 +29,9 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
     );
 
     /**
-     * @dev Sets the royalties for the contract
+     * @dev Sets the base royalties for the contract
+     * @param receivers The addresses that will receive royalties.
+     * @param basisPoints The basis points to calculate royalty payments (1/100th of a percent) for each receiver.
      */
     function setBaseRoyalties(
         address payable[] memory receivers,
@@ -32,6 +41,12 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
         emit TokenRoyaltiesUpdated(receivers, basisPoints);
     }
 
+    /**
+     * @dev Sets the token-specific royalties for a given token ID
+     * @param _tokenId The token ID for which the royalties are being set.
+     * @param receivers The addresses that will receive royalties.
+     * @param basisPoints The basis points to calculate royalty payments (1/100th of a percent) for each receiver.
+     */
     function setTokenRoyalties(
         uint256 _tokenId,
         address payable[] memory receivers,
@@ -41,6 +56,13 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
         emit TokenIdRoyaltiesUpdated(_tokenId, receivers, basisPoints);
     }
 
+    /**
+     * @dev Gets the royalty information for a given token ID and sale price
+     * @param _tokenId The token ID for which the royalty information is being retrieved.
+     * @param salePrice The sale price of the token.
+     * @return receiver The address that will receive the royalty payment.
+     * @return amount The amount of royalty payment in wei.
+     */
     function royaltyInfo(
         uint256 _tokenId,
         uint256 salePrice
@@ -57,6 +79,12 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
         return (receiver, amount);
     }
 
+    /**
+     * @dev Gets the royalty information for a given token ID
+     * @param _tokenId The token ID for which the royalty information is being retrieved.
+     * @return receivers The addresses that will receive royalties.
+     * @return basisPoints The basis points to calculate royalty payments (1/100th of a percent) for each receiver.
+     */
     function getRoyalties(
         uint256 _tokenId
     ) external view returns (address payable[] memory, uint256[] memory) {
@@ -79,6 +107,12 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
         return (allReceivers, allBasisPoints);
     }
 
+    /**
+     * @dev Sets the token-specific royalties for a given token ID
+     * @param _tokenId The token ID for which the royalties are being set.
+     * @param receivers The addresses that will receive royalties.
+     * @param basisPoints The basis points to calculate royalty payments (1/100th of a percent) for each receiver.
+     */
     function _setTokenRoyalties(
         uint256 _tokenId,
         address payable[] memory receivers,
@@ -108,15 +142,16 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
     }
 
     /**
-     * @dev Sets the royalties for the contract
-     * @param receivers "
-     * @param basisPoints "
+     * @dev Sets the base royalties for the contract
+     * @param receivers The addresses that will receive royalties.
+     * @param basisPoints The basis points to calculate royalty payments (1/100th of a percent) for each receiver.
      */
+    /// TODO: Move to like how manifold does it where its assumed that if you're setting royalties that youre first,
+    /// reseting them and then setting them
     function _setBaseRoyalties(
         address payable[] memory receivers,
         uint96[] memory basisPoints
     ) internal {
-        /// TODO: check for duplicates
         if (receivers.length != basisPoints.length) revert LengthMismatch();
         if (royalties.length != 0) revert RoyaltiesAlreadySet();
         _checkRoyalties(basisPoints);
@@ -129,14 +164,14 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
     /**
      * @dev Removes default royalty information.
      */
-
     function _resetBaseRoyalty() internal virtual {
         if (royalties.length == 0) revert RoyaltiesNotSet();
         delete royalties;
     }
 
     /**
-     * @dev Resets royalty information for the token id back to the global default.
+     * @dev Resets royalty information for the token ID back to the global default.
+     * @param _tokenId The token ID for which the royalties are being reset.
      */
     function _resetTokenRoyalty(uint256 _tokenId) internal virtual {
         RoyaltyInfo[] storage tokenRoyalties = royaltyTokenInfo[_tokenId];
@@ -145,6 +180,10 @@ abstract contract RoyaltyManager is IRoyaltyManager, IERC2981Upgradeable {
         delete royaltyTokenInfo[_tokenId];
     }
 
+    /**
+     * @dev Returns the fee denominator for calculating royalty amounts.
+     * @return The fee denominator.
+     */
     function _feeDenominator() internal pure virtual returns (uint96) {
         return 10000;
     }
