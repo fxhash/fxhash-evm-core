6585e4eb4062b2737b3dae7e0b68a3b5f98fd27f
swaHili <samehta91@gmail.com>
2023-07-28 12:24:32 -0700

    Update configuration manager (#45)
    * Update configuration manager
    
    * Fix failing tests
    
    * Add natspec
    
    * Update grammar
    
    * Add constants file
    
    * Add more constants
    
    * Sort

--- contracts/gentk/GenTk.sol
+++ contracts/gentk/GenTk.sol
@@ -54,7 +54,7 @@ contract GenTk is ERC721URIStorageUpgradeable, OwnableUpgradeable, IERC2981Upgra
     }
 
     modifier onlySigner() {
-        require(msg.sender == configManager.getAddress("signer"), "Caller is not signer");
+        require(msg.sender == configManager.contracts("signer"), "Caller is not signer");
         _;
     }
 
@@ -64,7 +64,7 @@ contract GenTk is ERC721URIStorageUpgradeable, OwnableUpgradeable, IERC2981Upgra
     }
 
     modifier onlyFxHashAdmin() {
-        require(msg.sender == configManager.getAddress("admin"), "Caller is not FXHASH admin");
+        require(msg.sender == configManager.contracts("admin"), "Caller is not FXHASH admin");
         _;
     }
 
@@ -114,7 +114,7 @@ contract GenTk is ERC721URIStorageUpgradeable, OwnableUpgradeable, IERC2981Upgra
         require(_tokenData.minter != address(0), "TOKEN_UNDEFINED");
         if (issuerData.onChainData.length > 0) {
             string memory onChainURI = IOnChainTokenMetadataManager(
-                configManager.getAddress("onChainMetaManager")
+                configManager.contracts("onChainMetaManager")
             ).getOnChainURI(bytes(_tokenURI), issuerData.onChainData);
             return onChainURI;
         } else {
