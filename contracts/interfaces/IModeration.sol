b2a4c239738ed95be7af7b4ae4bad869515c25cf
ciphrd <crespy.b@gmail.com>
2023-07-28 11:42:13 +0200

    added comments on moderation interface

--- contracts/interfaces/IModeration.sol
+++ contracts/interfaces/IModeration.sol
@@ -2,5 +2,12 @@
 pragma solidity ^0.8.18;
 
 interface IModeration {
+    /**
+     * Requests whether a given address has a given authorization (represented
+     * by an integer)
+     * @param userAddress address of the wallet to request
+     * @param authorization the authorization flag requested
+     * @return authorized whether the address has the authorization requested
+     */
     function isAuthorized(address userAddress, uint256 authorization) external view returns (bool);
 }
