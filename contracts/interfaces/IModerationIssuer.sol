4e0f511d9a3ccf3106ad7186325952cfe4a7583e
ciphrd <crespy.b@gmail.com>
2023-07-28 11:45:48 +0200

    added comments on the moderation interfaces

--- contracts/interfaces/IModerationIssuer.sol
+++ contracts/interfaces/IModerationIssuer.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.18;
 
 interface IModerationIssuer {
+    /**
+     * Requests the state held by an issuer contract. The state can be changed
+     * by moderators to prevent minting for instance (if the project is not
+     * respecting guidelines in place). If no state specified by a moderator,
+     * returns 0 (default state = NONE)
+     * @param issuerContract the address of the issuer requested
+     * @return state numeric identifier of the state
+     */
     function issuerState(address issuerContract) external view returns (uint256);
 }
--- contracts/interfaces/IModerationUser.sol
+++ contracts/interfaces/IModerationUser.sol
@@ -2,5 +2,14 @@
 pragma solidity ^0.8.18;
 
 interface IModerationUser {
+    /**
+     * Requests the state held by a wallet. The state can be changed by 
+     * moderators to prevent access to certain features, if the user has 
+     * infringed some guidelines of the platform, for instance. It also returns
+     * if a user is verified (state = 10) .If no state specified by a moderator,
+     * returns 0 (default state = NONE)
+     * @param user the address of the issuer requested
+     * @return state numeric identifier of the state
+     */
     function userState(address user) external view returns (uint256);
 }
