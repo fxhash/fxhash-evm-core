3d230ef9775e901e07cacf9ba6b4a3820eadbf02
steven nevins <steven.nevins.js@gmail.com>
2023-08-07 10:25:55 -0400

    clean up mintpass and rename merkle mint

--- contracts/minters/MintPass.sol
+++ contracts/minters/MintPass.sol
@@ -1,22 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import {IMinter} from "contracts/interfaces/IMinter.sol";
-import {Minted, Reserve} from "contracts/minters/base/Minted.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract MintPass is IMinter {
+contract MintPass {
     using ECDSA for bytes32;
 
-    address FXHASH_AUTHORITY;
+    address internal FXHASH_AUTHORITY;
     mapping(address => mapping(uint256 => uint256)) public redeemedBitMaps;
 
     error AlreadyClaimed();
     error InvalidSig();
-
-    /// should integrate delegate cash
-
-    function setMintDetails(Reserve calldata _reserve, bytes calldata) external {}
 
     function isClaimed(address _token, uint256 _index) public view returns (bool) {
         uint256 claimedWordIndex = _index / 256;
@@ -26,20 +20,16 @@ contract MintPass is IMinter {
         return claimedWord & mask == mask;
     }
 
-    function mint(
+    function _claimMintPass(
         address _token,
-        address _redeemer,
-        /// this should be msg.sender or vault
         uint256 _index,
         bytes calldata _mintCode,
-        bytes calldata sig,
-        address _to
-    ) external {
+        bytes calldata _sig
+    ) internal {
         if (isClaimed(_token, _index)) revert AlreadyClaimed();
         bytes32 hash = keccak256(abi.encodePacked(_token, msg.sender, _index, _mintCode));
-        if (hash.toEthSignedMessageHash().recover(sig) != FXHASH_AUTHORITY) revert InvalidSig();
+        if (hash.toEthSignedMessageHash().recover(_sig) != FXHASH_AUTHORITY) revert InvalidSig();
         _setClaimed(_token, _index);
-        Minted(_token).mint(1, _mintCode, _to);
     }
 
     function _setClaimed(address _token, uint256 _index) private {
