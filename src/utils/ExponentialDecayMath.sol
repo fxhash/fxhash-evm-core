189fba1c4e92178532e2c2f8ea02fd0e94ec0a0a
steven nevins <steven.nevins.js@gmail.com>
2023-08-17 15:57:36 -0400

    clean up comments

--- src/utils/ExponentialDecayMath.sol
+++ src/utils/ExponentialDecayMath.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.20;
 
 import {wadExp, wadLn, wadMul, unsafeWadMul} from "solmate/src/utils/SignedWadMath.sol";
 
-/// @notice Calculate the price of a token according to a decaying price schedule.
+/// @notice Calculates the remaining price after an amount of exponential decay.
 /// @dev decayConstant must be non-negative
-/// @param initialPrice The target price for a token, scaled by 1e18.
+/// @param initialPrice The starting price, scaled by 1e18.
 /// @param perTimeUnitDecay The percent price decays per unit of time, scaled by 1e18.
-/// @param timeSinceStart Time passed since the DA began, scaled by 1e18.
-/// @return The price of a token according to DA, scaled by 1e18.
+/// @param timeSinceStart Time passed since the initial price began decaying, scaled by 1e18.
+/// @return The price remaining after some amount of decay, scaled by 1e18.
 function calculateExponentialDecay(
     int256 initialPrice,
     int256 perTimeUnitDecay,
