24709120e38d95701af326dc041a6661b582b5c9
steven nevins <steven.nevins.js@gmail.com>
2023-08-18 09:31:55 -0400

    use inherit doc

--- src/FxRandomizer.sol
+++ src/FxRandomizer.sol
@@ -7,10 +7,7 @@ import {IFxSeedConsumer} from "src/interfaces/IFxSeedConsumer.sol";
 /// @title FxRandomizer
 /// @notice See documentation in {IFxRandomizer}
 contract FxRandomizer is IFxRandomizer {
-    /**
-     * @notice Requests a seed for a given token ID
-     * @param _tokenId The ID of the token to request a seed for
-     */
+    ///  @inheritdoc IFxRandomizer
     function requestRandomness(uint256 _tokenId) external {
         // Generate a pseudo-random seed using the block hash, sender address, and token ID
         bytes32 seed = keccak256(
@@ -18,16 +15,6 @@ contract FxRandomizer is IFxRandomizer {
         );
 
         // Call the requester's fulfillSeedRequest function with the generated seed
-        _requestCallback(msg.sender, _tokenId, seed);
-    }
-
-    /**
-     * @notice Calls the requester's fulfillSeedRequest function with the generated seed
-     * @param _requester The address of the requester
-     * @param _tokenId The ID of the token that the seed was generated for
-     * @param _seed The generated seed
-     */
-    function _requestCallback(address _requester, uint256 _tokenId, bytes32 _seed) internal {
-        IFxSeedConsumer(_requester).fulfillSeedRequest(_tokenId, _seed);
+        IFxSeedConsumer(msg.sender).fulfillSeedRequest(_tokenId, seed);
     }
 }
