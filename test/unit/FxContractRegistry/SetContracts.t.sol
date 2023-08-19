28dc5d3cba0967b782971db83463091708ea1bc6
steven nevins <steven.nevins.js@gmail.com>
2023-08-19 16:09:06 -0400

    add some tests for the contract registry

--- test/unit/FxContractRegistry/SetContracts.t.sol
+++ test/unit/FxContractRegistry/SetContracts.t.sol
@@ -4,11 +4,46 @@ pragma solidity ^0.8.18;
 import {FxContractRegistryTest} from "test/unit/FxContractRegistry/FxContractRegistryTest.sol";
 
 contract SetContracts is FxContractRegistryTest {
+    string[] internal names;
+    address[] internal contracts;
+
     function setUp() public virtual override {
         super.setUp();
+        names.push("Test");
+        contracts.push(address(420));
     }
 
-    function test_True() public {
-        assertTrue(true);
+    function test_SetContracts() public {
+        registry.setContracts(names, contracts);
+    }
+
+    function test_RevertsWhen_EmptyArrays() public {
+        delete names;
+        delete contracts;
+
+        vm.expectRevert();
+        registry.setContracts(names, contracts);
+    }
+
+    function test_RevertsWhen_ArrayLengthMismatch() public {
+        names.push("Test_2");
+
+        vm.expectRevert();
+        registry.setContracts(names, contracts);
+    }
+
+    function test_RevertsWhen_ContractAlreadySet() public {
+        names.push("Test");
+        contracts.push(address(69));
+
+        vm.expectRevert();
+        registry.setContracts(names, contracts);
+    }
+
+    function test_RevertsWhen_ContractAddress0() public {
+        contracts[0] = address(0);
+
+        vm.expectRevert();
+        registry.setContracts(names, contracts);
     }
 }
