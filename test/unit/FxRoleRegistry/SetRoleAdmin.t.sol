be6b7547d8a415f121b889ce44929a66b4e82d6a
steven nevins <steven.nevins.js@gmail.com>
2023-08-20 10:56:24 -0400

    add tests

--- test/unit/FxRoleRegistry/SetRoleAdmin.t.sol
+++ test/unit/FxRoleRegistry/SetRoleAdmin.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {FxRoleRegistryTest} from "test/unit/FxRoleRegistry/FxRoleRegistryTest.sol";
+import {ADMIN_ROLE, CREATOR_ROLE, MINTER_ROLE, MODERATOR_ROLE} from "src/utils/Constants.sol";
 
 contract SetRoleAdmin is FxRoleRegistryTest {
     function setUp() public virtual override {
@@ -9,6 +10,19 @@ contract SetRoleAdmin is FxRoleRegistryTest {
     }
 
     function test_SetRoleAdmin() public {
-        assertTrue(true);
+        registry.setRoleAdmin(ADMIN_ROLE);
+    }
+
+    function test_RevertsWhen_NotDefaultAdmin() public {
+        vm.prank(address(420));
+        vm.expectRevert();
+        registry.setRoleAdmin(ADMIN_ROLE);
+    }
+
+    function test_RevertsWhen_NotRoleAdmin() public {
+        /// in this case address(this) is the default admin as well as admin
+        vm.prank(address(420));
+        vm.expectRevert();
+        registry.setRoleAdmin(ADMIN_ROLE);
     }
 }
