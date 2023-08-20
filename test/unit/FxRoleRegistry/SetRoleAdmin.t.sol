05ab750f98d054c50d0d70aba78d62dc1ebc45c0
steven nevins <steven.nevins.js@gmail.com>
2023-08-20 18:57:53 -0400

    explicitly check for revert string

--- test/unit/FxRoleRegistry/SetRoleAdmin.t.sol
+++ test/unit/FxRoleRegistry/SetRoleAdmin.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.18;
 
 import {FxRoleRegistryTest} from "test/unit/FxRoleRegistry/FxRoleRegistryTest.sol";
 import {ADMIN_ROLE} from "src/utils/Constants.sol";
+import {Strings} from "openzeppelin/contracts/utils/Strings.sol";
 
 contract SetRoleAdmin is FxRoleRegistryTest {
     function test_SetRoleAdmin() public {
@@ -12,7 +13,14 @@ contract SetRoleAdmin is FxRoleRegistryTest {
 
     function test_RevertsWhen_NotDefaultAdmin() public {
         vm.prank(alice);
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodePacked(
+                "AccessControl: account ",
+                Strings.toHexString(alice),
+                " is missing role ",
+                Strings.toHexString(uint256(ADMIN_ROLE), 32)
+            )
+        );
         fxRoleRegistry.setRoleAdmin(ADMIN_ROLE);
         assertFalse(fxRoleRegistry.hasRole(ADMIN_ROLE, alice));
     }
@@ -20,7 +28,14 @@ contract SetRoleAdmin is FxRoleRegistryTest {
     function test_RevertsWhen_NotRoleAdmin() public {
         /// in this case address(this) is the default admin as well as admin
         vm.prank(alice);
-        vm.expectRevert();
+        vm.expectRevert(
+            abi.encodePacked(
+                "AccessControl: account ",
+                Strings.toHexString(alice),
+                " is missing role ",
+                Strings.toHexString(uint256(ADMIN_ROLE), 32)
+            )
+        );
         fxRoleRegistry.setRoleAdmin(ADMIN_ROLE);
         assertFalse(fxRoleRegistry.hasRole(ADMIN_ROLE, alice));
     }
