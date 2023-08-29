ea8a2245283c43a2893f34e6396e3da5564ed0df
steven nevins <steven.nevins.js@gmail.com>
2023-08-29 08:02:58 -0400

    named variables

--- test/unit/utils/MintPass-EIP712/ClaimMintPass.t.sol
+++ test/unit/utils/MintPass-EIP712/ClaimMintPass.t.sol
@@ -7,6 +7,8 @@ import {MockMintPass} from "test/mocks/MockMintPass-EIP712.sol";
 
 contract MintPass712Test is BaseTest {
     MockMintPass internal mintPass;
+    uint256 internal claimIndex = 1;
+    address internal claimerAddress = address(this);
     uint256 internal signerPk = 1;
     address internal signerAddress = vm.addr(signerPk);
 
@@ -15,41 +17,41 @@ contract MintPass712Test is BaseTest {
     }
 
     function test_SignMintPass() public {
-        bytes32 digest = mintPass.genTypedDataHash(1, address(this), "");
+        bytes32 digest = mintPass.genTypedDataHash(claimIndex, claimerAddress, "");
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk, digest);
         address signer = ECDSA.recover(digest, v, r, s);
         assertTrue(signer == signerAddress, "Invalid Sig");
     }
 
     function test_RevertsWhen_SignatureInvalid_SignMintPass() public {
-        bytes32 digest = mintPass.genTypedDataHash(1, address(this), "");
+        bytes32 digest = mintPass.genTypedDataHash(claimIndex, claimerAddress, "");
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk + 1, digest);
         address signer = ECDSA.recover(digest, v, r, s);
         assertFalse(signer == signerAddress, "Signature was Valid");
     }
 
     function test_ClaimMintPass() public {
-        bytes32 digest = mintPass.genTypedDataHash(1, address(this), "");
+        bytes32 digest = mintPass.genTypedDataHash(claimIndex, claimerAddress, "");
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk, digest);
-        mintPass.claimMintPass(1, "", abi.encode(v, r, s));
+        mintPass.claimMintPass(claimIndex, "", abi.encode(v, r, s));
         assertTrue(mintPass.isClaimed(1), "Mint pass not claimed");
     }
 
     function test_RevertsWhen_AlreadyClaimed_ClaimMintPass() public {
-        bytes32 digest = mintPass.genTypedDataHash(1, address(this), "");
+        bytes32 digest = mintPass.genTypedDataHash(claimIndex, claimerAddress, "");
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk, digest);
-        mintPass.claimMintPass(1, "", abi.encode(v, r, s));
+        mintPass.claimMintPass(claimIndex, "", abi.encode(v, r, s));
         assertTrue(mintPass.isClaimed(1), "Mint pass not claimed");
         vm.expectRevert();
-        mintPass.claimMintPass(1, "", abi.encode(v, r, s));
+        mintPass.claimMintPass(claimIndex, "", abi.encode(v, r, s));
     }
 
     function test_RevertsWhen_NotClaimer_ClaimMintPass() public {
-        bytes32 digest = mintPass.genTypedDataHash(1, address(this), "");
+        bytes32 digest = mintPass.genTypedDataHash(claimIndex, claimerAddress, "");
         (uint8 v, bytes32 r, bytes32 s) = vm.sign(signerPk, digest);
         vm.prank(address(bob));
         vm.expectRevert();
-        mintPass.claimMintPass(1, "", abi.encode(v, r, s));
+        mintPass.claimMintPass(claimIndex, "", abi.encode(v, r, s));
         assertTrue(!mintPass.isClaimed(1), "Mint was claimed");
     }
 }
