45791b773cbe3683265f0251f19d165738fdae6b
steven <12021290+stevennevins@users.noreply.github.com>
2023-08-30 15:43:17 -0400

    Feat: Mint Pass Signatures (#74)
    * add mint pass implementation
    
    * implement eip712 mint pass
    
    * add natspec to the eip712 mintpass implementation
    
    * fix comment
    
    * make mint passes more generic
    
    * setup tests
    
    * rename
    
    * more setup
    
    * make mock contract
    
    * mocks
    
    * test mock mint pass
    
    * only use eip712 mint pass
    
    * named variables
    
    * clean up
    
    * use state vars
    
    * add explicit revert checks
    
    * rename
    
    * move claim typehash to constants
    
    * refactor to handle integrate role registry check
    
    * compiler version and license
    
    * rename _genTypedDataHash and make public
    
    * clean up natspec
    
    * restructure directory
    
    * switch to actual eip712

--- test/unit/FxContractRegistry/SetContracts.t.sol
+++ test/unit/FxContractRegistry/SetContracts.t.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import {FxContractRegistryTest} from "test/unit/FxContractRegistry/FxContractRegistryTest.sol";
 import {IFxContractRegistry} from "src/interfaces/IFxContractRegistry.sol";
