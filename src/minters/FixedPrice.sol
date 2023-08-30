c960a7b658e6f1c1a3dbd07e94169260e5990384
steven nevins <steven.nevins.js@gmail.com>
2023-08-29 20:44:03 -0400

    use reserveInfo in token interface

--- src/minters/FixedPrice.sol
+++ src/minters/FixedPrice.sol
@@ -1,22 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.18;
 
-import {IFxGenArt721} from "src/interfaces/IFxGenArt721.sol";
+import {IFxGenArt721, ReserveInfo} from "src/interfaces/IFxGenArt721.sol";
 import {SafeCastLib} from "solmate/src/utils/SafeCastLib.sol";
 import {SafeTransferLib} from "solmate/src/utils/SafeTransferLib.sol";
-
-struct Reserve {
-    uint160 allocation;
-    uint40 startTime;
-    uint40 endTime;
-}
 
 contract FixedPrice {
     using SafeCastLib for uint256;
 
-    bytes32 internal constant NULL_RESERVE = keccak256(abi.encode(Reserve(0, 0, 0)));
+    bytes32 internal constant NULL_RESERVE = keccak256(abi.encode(ReserveInfo(0, 0, 0)));
     mapping(address => uint256[]) public prices;
-    mapping(address => Reserve[]) public reserves;
+    mapping(address => ReserveInfo[]) public reserves;
     mapping(address => uint256) public saleProceeds;
 
     error InvalidToken();
@@ -25,7 +19,7 @@ contract FixedPrice {
     error Ended();
     error TooMany();
 
-    function setMintDetails(Reserve calldata _reserve, bytes calldata _mintDetails) external {
+    function setMintDetails(ReserveInfo calldata _reserve, bytes calldata _mintDetails) external {
         uint256 price = abi.decode(_mintDetails, (uint256));
         prices[msg.sender].push(price);
         reserves[msg.sender].push(_reserve);
@@ -35,14 +29,14 @@ contract FixedPrice {
         external
         payable
     {
-        Reserve storage reserve = reserves[_token][_mintId];
+        ReserveInfo storage reserve = reserves[_token][_mintId];
         if (NULL_RESERVE == keccak256(abi.encode(reserve))) revert InvalidToken();
         if (block.timestamp < reserve.startTime) revert NotStarted();
         if (block.timestamp > reserve.endTime) revert Ended();
         if (_amount > reserve.allocation) revert TooMany();
         uint256 price = _amount * prices[_token][_mintId];
         if (msg.value != price) revert InvalidPrice();
-        reserve.allocation -= _amount.safeCastTo160();
+        reserve.allocation -= _amount.safeCastTo128();
         saleProceeds[_token] += price;
         IFxGenArt721(_token).mint(_to, _amount);
     }
