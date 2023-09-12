1fe530e61fb9263183ebc631a5b961180a866f79
steven <12021290+stevennevins@users.noreply.github.com>
2023-09-11 21:41:01 -0400

    Feat: Allowlist (#81)
    * initial commit for allowlist
    
    * add title
    
    * remove isClaimed
    
    * lay out tests
    
    * test allowlist
    
    * ignore lcov
    
    * add multiple claim test
    
    * move file and rename, and move storage to base allowlist test
    
    * refactor creating merkle tree
    
    * dev -> notice and clarity in comment
    
    * rename functions
    
    * sort and implicit imports
    
    * rename merkle.sol
    
    * add asserts
    
    * Clean up
    
    ---------
    
    Co-authored-by: Sahil Mehta <samehta91@gmail.com>

---
+++ test/mocks/MockAllowlist.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import {Allowlist} from "src/minters/extensions/Allowlist.sol";
+import {BitMaps} from "openzeppelin/contracts/utils/structs/BitMaps.sol";
+
+contract MockAllowlist is Allowlist {
+    using BitMaps for BitMaps.BitMap;
+
+    BitMaps.BitMap internal _bitmap;
+    bytes32 public merkleRoot;
+
+    function claimSlot(address _token, uint256 _index, uint256 _price, bytes32[] memory _proof)
+        external
+    {
+        _claimSlot(_bitmap, _token, _index, _price, _proof);
+    }
+
+    function setMerkleRoot(bytes32 _merkleRoot) external {
+        merkleRoot = _merkleRoot;
+    }
+
+    function isClaimed(uint256 _index) external view returns (bool) {
+        return _bitmap.get(_index);
+    }
+
+    function _getMerkleRoot(address /* _token */ ) internal view override returns (bytes32) {
+        return merkleRoot;
+    }
+}
