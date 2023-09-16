eb88ea4ba32b1bb9d01a4b5b85019f65905f8638
steven nevins <steven.nevins.js@gmail.com>
2023-09-16 19:37:46 -0400

    use alice in tests

--- test/unit/FixedPrice/Buy.t.sol
+++ test/unit/FixedPrice/Buy.t.sol
@@ -6,40 +6,40 @@ import "test/unit/FixedPrice/FixedPrice.t.sol";
 contract BuyTokens is FixedPriceTest {
     function test_buy() public {
         vm.warp(block.timestamp);
-        sale.buy{value: price}(address(mockToken), 0, 1, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 1);
+        sale.buy{value: price}(address(mockToken), 0, 1, alice);
+        assertEq(mockToken.balanceOf(alice), 1);
     }
 
     function test_RevertsIf_BuyMoreThanAllocation() public {
         vm.expectRevert(abi.encodeWithSelector(IFixedPrice.TooMany.selector));
-        sale.buy{value: (price * (supply + 1))}(address(mockToken), 0, supply + 1, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 0);
+        sale.buy{value: (price * (supply + 1))}(address(mockToken), 0, supply + 1, alice);
+        assertEq(mockToken.balanceOf(alice), 0);
     }
 
     function test_RevertsIf_InsufficientPayent() public {
         vm.expectRevert(abi.encodeWithSelector(IFixedPrice.InvalidPayment.selector));
-        sale.buy{value: price - 1}(address(mockToken), 0, 1, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 0);
+        sale.buy{value: price - 1}(address(mockToken), 0, 1, alice);
+        assertEq(mockToken.balanceOf(alice), 0);
     }
 
     function test_RevertsIf_NotStarted() public {
         vm.warp(block.timestamp - 1);
         vm.expectRevert(abi.encodeWithSelector(IFixedPrice.NotStarted.selector));
-        sale.buy{value: price}(address(mockToken), 0, 1, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 0);
+        sale.buy{value: price}(address(mockToken), 0, 1, alice);
+        assertEq(mockToken.balanceOf(alice), 0);
     }
 
     function test_RevertsIf_Ended() public {
         vm.warp(uint256(endTime) + 1);
         vm.expectRevert(abi.encodeWithSelector(IFixedPrice.Ended.selector));
-        sale.buy{value: price}(address(mockToken), 0, 1, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 0);
+        sale.buy{value: price}(address(mockToken), 0, 1, alice);
+        assertEq(mockToken.balanceOf(alice), 0);
     }
 
     function test_RevertsIf_TokenAddress0() public {
         vm.expectRevert(abi.encodeWithSelector(IFixedPrice.InvalidToken.selector));
-        sale.buy{value: price}(address(0), 0, 1, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 0);
+        sale.buy{value: price}(address(0), 0, 1, alice);
+        assertEq(mockToken.balanceOf(alice), 0);
     }
 
     function test_RevertsIf_ToAddress0() public {
@@ -49,7 +49,7 @@ contract BuyTokens is FixedPriceTest {
 
     function test_RevertsIf_Purchase0() public {
         vm.expectRevert();
-        sale.buy{value: price}(address(mockToken), 0, 0, address(this));
-        assertEq(mockToken.balanceOf(address(this)), 0);
+        sale.buy{value: price}(address(mockToken), 0, 0, alice);
+        assertEq(mockToken.balanceOf(alice), 0);
     }
 }
--- test/unit/FixedPrice/Withdraw.t.sol
+++ test/unit/FixedPrice/Withdraw.t.sol
@@ -5,7 +5,7 @@ import "test/unit/FixedPrice/FixedPrice.t.sol";
 
 contract Withdraw is FixedPriceTest {
     function test_withdraw() public {
-        sale.buy{value: price}(address(mockToken), 0, 1, address(this));
+        sale.buy{value: price}(address(mockToken), 0, 1, alice);
         uint256 beforeBalance = address(this).balance;
         sale.withdraw(address(mockToken));
         uint256 afterBalance = address(this).balance;
@@ -13,7 +13,7 @@ contract Withdraw is FixedPriceTest {
     }
 
     function test_RevertsIf_Token0() public {
-        sale.buy{value: price}(address(mockToken), 0, 1, address(this));
+        sale.buy{value: price}(address(mockToken), 0, 1, alice);
         vm.expectRevert(abi.encodeWithSelector(IFixedPrice.InsufficientFunds.selector));
         sale.withdraw(address(0));
     }
