0b33dfe8e1b8a649559518092e4100961b3b9093
steven nevins <steven.nevins.js@gmail.com>
2023-09-16 19:31:30 -0400

    move receive to base teset

--- test/unit/FixedPrice/Withdraw.t.sol
+++ test/unit/FixedPrice/Withdraw.t.sol
@@ -4,8 +4,6 @@ pragma solidity ^0.8.20;
 import "test/unit/FixedPrice/FixedPrice.t.sol";
 
 contract Withdraw is FixedPriceTest {
-    receive() external payable {}
-
     function test_withdraw() public {
         sale.buy{value: price}(address(mockToken), 0, 1, address(this));
         sale.withdraw(address(mockToken));
