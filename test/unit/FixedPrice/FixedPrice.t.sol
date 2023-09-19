299f9a6f6fe4aa8549012ef77d29f12b5f9e2fb5
steven nevins <steven.nevins.js@gmail.com>
2023-09-19 19:19:16 -0400

    remove redudant import

--- test/unit/FixedPrice/FixedPrice.t.sol
+++ test/unit/FixedPrice/FixedPrice.t.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.20;
 import "test/BaseTest.t.sol";
 import {IFixedPrice} from "src/interfaces/IFixedPrice.sol";
 import {FixedPrice} from "src/minters/FixedPrice.sol";
-import {ReserveInfo} from "src/interfaces/IFxGenArt721.sol";
 
 contract FixedPriceTest is BaseTest {
     FixedPrice internal sale;
