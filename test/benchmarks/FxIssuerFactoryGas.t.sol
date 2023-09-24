62205e6a635ef7f1f22a533f8bd2844c1ea723e4
steven nevins <steven.nevins.js@gmail.com>
2023-09-24 12:53:35 -0400

    add offchain test

--- test/benchmarks/FxIssuerFactoryGas.t.sol
+++ test/benchmarks/FxIssuerFactoryGas.t.sol
@@ -9,7 +9,24 @@ contract FxIssuerFactoryGas is Deploy {
         _deployContracts();
     }
 
-    function test_createProject() public {
+    function test_Onchain_createProject() public {
+        vm.prank(creator);
+        fxGenArtProxy = fxIssuerFactory.createProject(
+            creator,
+            address(this),
+            projectInfo,
+            metadataInfo,
+            mintInfo,
+            royaltyReceivers,
+            basisPoints
+        );
+    }
+
+    function test_Offchain_createProject() public {
+        vm.pauseGasMetering();
+        projectInfo.onchain = false;
+        delete metadataInfo.animation;
+        vm.resumeGasMetering();
         vm.prank(creator);
         fxGenArtProxy = fxIssuerFactory.createProject(
             creator,
