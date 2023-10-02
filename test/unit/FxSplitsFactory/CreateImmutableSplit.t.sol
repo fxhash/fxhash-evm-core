feb843d1ebfd22ec28c423de1e19a7a02f48a335
steven <12021290+stevennevins@users.noreply.github.com>
2023-10-02 12:02:23 -0400

    Feat: Add Splits Controller (#117)
    * add splits controller
    
    * rename
    
    * add owner check
    
    * add view function for fxSplitsController to interface
    
    * indexed controller
    
    * alice and bob and update controller
    
    * fix formatting
    
    ---------
    
    Co-authored-by: swaHili <samehta91@gmail.com>

--- test/unit/FxSplitsFactory/CreateImmutableSplit.t.sol
+++ test/unit/FxSplitsFactory/CreateImmutableSplit.t.sol
@@ -6,33 +6,33 @@ import "test/unit/FxSplitsFactory/FxSplitsFactoryTest.sol";
 contract CreateSplit is FxSplitsFactoryTest {
     function setUp() public virtual override {
         super.setUp();
-        accounts.push(address(2));
-        accounts.push(address(3));
-        allocations.push(uint32(400_000));
+        accounts.push(bob);
+        accounts.push(alice);
         allocations.push(uint32(600_000));
+        allocations.push(uint32(400_000));
     }
 
-    function test_createSplit() public {
-        fxSplitsFactory.createSplit(accounts, allocations);
+    function test_createImmutableSplit() public {
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 
     function test_RevertsWhen_CreateSplitTwice() public {
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
         vm.expectRevert(abi.encodeWithSelector(IFxSplitsFactory.SplitsExists.selector));
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 
     function test_FirstWithdraw() public {
         address libPredicted =
             ISplitsMain(SPLITS_MAIN).predictImmutableSplitAddress(accounts, allocations, 0);
         vm.deal(libPredicted, 1 ether);
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
         ISplitsMain(SPLITS_MAIN).distributeETH(libPredicted, accounts, allocations, 0, address(0));
-        ISplitsMain(SPLITS_MAIN).withdraw(address(2), 0.4 ether, new address[](0));
-        ISplitsMain(SPLITS_MAIN).withdraw(address(3), 0.6 ether, new address[](0));
+        ISplitsMain(SPLITS_MAIN).withdraw(alice, 0.4 ether, new address[](0));
+        ISplitsMain(SPLITS_MAIN).withdraw(bob, 0.6 ether, new address[](0));
         /// on first withdraw, 1 wei is withheld for gas savings
-        assertGt(address(2).balance, 0.3999999 ether);
-        assertGt(address(3).balance, 0.5999999 ether);
+        assertGt(alice.balance, 0.3999999 ether);
+        assertGt(bob.balance, 0.5999999 ether);
     }
 
     function test_2ndWithdraw() public {
@@ -43,20 +43,20 @@ contract CreateSplit is FxSplitsFactoryTest {
         ISplitsMain(SPLITS_MAIN).distributeETH(
             computedAddress, accounts, allocations, 0, address(0)
         );
-        uint256 cachedBalance2 = address(2).balance;
-        uint256 cachedBalance3 = address(3).balance;
-        ISplitsMain(SPLITS_MAIN).withdraw(address(2), 0.4 ether, new address[](0));
-        ISplitsMain(SPLITS_MAIN).withdraw(address(3), 0.6 ether, new address[](0));
+        uint256 cachedBalance2 = alice.balance;
+        uint256 cachedBalance3 = bob.balance;
+        ISplitsMain(SPLITS_MAIN).withdraw(alice, 0.4 ether, new address[](0));
+        ISplitsMain(SPLITS_MAIN).withdraw(bob, 0.6 ether, new address[](0));
         /// balance fully availalble
-        assertEq(address(2).balance - cachedBalance2, 0.4 ether);
-        assertEq(address(3).balance - cachedBalance3, 0.6 ether);
+        assertEq(alice.balance - cachedBalance2, 0.4 ether);
+        assertEq(bob.balance - cachedBalance3, 0.6 ether);
     }
 
     function test_RevertsWhen_LengthMismatch() public {
         accounts.pop();
 
         vm.expectRevert();
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 
     function test_RevertsWhen_AllocationsGt100() public {
@@ -64,29 +64,29 @@ contract CreateSplit is FxSplitsFactoryTest {
         allocations.push(1);
 
         vm.expectRevert();
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 
     function test_RevertsWhen_AllocationsLt100() public {
         allocations[0]--;
 
         vm.expectRevert();
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 
     function test_RevertsWhen_DuplicateAccountInAccounts() public {
-        accounts.push(address(2));
+        accounts.push(alice);
         allocations.push(1);
         allocations[0]--;
 
         vm.expectRevert();
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 
     function test_RevertsWhen_AccountsNotSorted() public {
         (accounts[0], accounts[1]) = (accounts[1], accounts[0]);
 
         vm.expectRevert();
-        fxSplitsFactory.createSplit(accounts, allocations);
+        fxSplitsFactory.createImmutableSplit(accounts, allocations);
     }
 }
