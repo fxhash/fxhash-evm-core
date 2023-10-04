dc37f73fd8366ef1aabd54ef3c21b25d6e1b38c6
steven nevins <steven.nevins.js@gmail.com>
2023-10-04 07:45:32 -0400

    fix compiler version to unique version

--- test/unit/DutchAuction/DutchAuctionTest.t.sol
+++ test/unit/DutchAuction/DutchAuctionTest.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.8.20;
 
 import "test/BaseTest.t.sol";
 import {IDutchAuction, AuctionInfo} from "src/interfaces/IDutchAuction.sol";
