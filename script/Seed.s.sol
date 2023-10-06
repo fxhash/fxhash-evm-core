7bd93a1583f0d7c66bd7b9066660102b91d4cc7c
Sahil Mehta <samehta91@gmail.com>
2023-10-06 15:27:45 -0700

    Update seed script

--- script/Seed.s.sol
+++ script/Seed.s.sol
@@ -6,20 +6,28 @@ import "script/Deploy.s.sol";
 contract Seed is Deploy {
     function _run() internal override {
         super._run();
-        creator = msg.sender;
+        _initializeState();
         for (uint256 i; i < 20; i++) {
-            _configure();
+            _createProject();
+            _setContracts();
             _mint();
         }
     }
 
-    function _configure() internal {
-        _createProject();
-        _setContracts();
-        IFxGenArt721(fxGenArtProxy).toggleMint();
+    function _initializeState() internal {
+        creator = msg.sender;
+        delete mintInfo;
+        _configureMinter(
+            address(fixedPrice),
+            uint64(block.timestamp) + RESERVE_START_TIME,
+            uint64(block.timestamp) + RESERVE_END_TIME,
+            MINTER_ALLOCATION,
+            abi.encode(PRICE)
+        );
     }
 
     function _mint() internal {
+        IFxGenArt721(fxGenArtProxy).toggleMint();
         for (uint256 i; i < 20; i++) {
             IFxGenArt721(fxGenArtProxy).ownerMint(creator);
         }
