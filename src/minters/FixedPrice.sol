3a79c8f323f89a0fca3a47bd4e1eae00c234cf33
steven nevins <steven.nevins.js@gmail.com>
2023-10-12 21:20:30 -0400

    update tests

--- src/minters/FixedPrice.sol
+++ src/minters/FixedPrice.sol
@@ -66,26 +66,13 @@ contract FixedPrice is MintPass, Allowlist, IFixedPrice {
         bytes32 merkleRoot = _getMerkleRoot(_token, _reserveId);
         address signer = signingAuthorities[_token][_reserveId];
         if ((merkleRoot != bytes32(0) || signer != address(0))) revert NoPublicMint();
-        uint256 length = reserves[_token].length;
-        if (length == 0) revert InvalidToken();
-        if (_reserveId >= length) revert InvalidReserve();
-        ReserveInfo storage reserve = reserves[_token][_reserveId];
-        if (block.timestamp < reserve.startTime) revert NotStarted();
-        if (block.timestamp > reserve.endTime) revert Ended();
-        if (_amount > reserve.allocation) revert TooMany();
-        if (_to == address(0)) revert AddressZero();
-        uint256 price = _amount * prices[_token][_reserveId];
-        if (msg.value != price) revert InvalidPayment();
-        reserve.allocation -= _amount.safeCastTo128();
-        saleProceeds[_token] += price;
-        IFxGenArt721(_token).mintRandom(_to, _amount);
-        emit Purchase(_token, _reserveId, msg.sender, _amount, _to, price);
+        _buy(_token, _reserveId, _amount, _to);
     }
 
     function buyAllowlist(
         address _token,
         uint256 _reserveId,
-        uint256 _index,
+        uint256[] calldata _indexes,
         address _to,
         bytes32[][] calldata _proofs
     ) external payable {
@@ -93,7 +80,7 @@ contract FixedPrice is MintPass, Allowlist, IFixedPrice {
         if (merkleRoot == bytes32(0)) revert NoAllowlist();
         BitMaps.BitMap storage claimBitmap = claimedMerkleTreeSlots[_token][_reserveId];
         for (uint256 i; i < _proofs.length; i++) {
-            _claimSlot(claimBitmap, _token, _reserveId, _index, _proofs[i]);
+            _claimSlot(claimBitmap, _token, _reserveId, _indexes[i], _proofs[i]);
         }
         uint256 amount = _proofs.length;
         _buy(_token, _reserveId, amount, _to);
@@ -124,7 +111,22 @@ contract FixedPrice is MintPass, Allowlist, IFixedPrice {
         emit Withdrawn(_token, saleReceiver, proceeds);
     }
 
-    function _buy(address _token, uint256 _reserveId, uint256 _amount, address _to) internal {}
+    function _buy(address _token, uint256 _reserveId, uint256 _amount, address _to) internal {
+        uint256 length = reserves[_token].length;
+        if (length == 0) revert InvalidToken();
+        if (_reserveId >= length) revert InvalidReserve();
+        ReserveInfo storage reserve = reserves[_token][_reserveId];
+        if (block.timestamp < reserve.startTime) revert NotStarted();
+        if (block.timestamp > reserve.endTime) revert Ended();
+        if (_amount > reserve.allocation) revert TooMany();
+        if (_to == address(0)) revert AddressZero();
+        uint256 price = _amount * prices[_token][_reserveId];
+        if (msg.value != price) revert InvalidPayment();
+        reserve.allocation -= _amount.safeCastTo128();
+        saleProceeds[_token] += price;
+        IFxGenArt721(_token).mintRandom(_to, _amount);
+        emit Purchase(_token, _reserveId, msg.sender, _amount, _to, price);
+    }
 
     function _getMerkleRoot(address _token, uint256 _reserveId) internal view override returns (bytes32) {
         return merkleRoots[_token][_reserveId];
--- test/minters/FixedPrice/BuyAllowlist.t.sol
+++ test/minters/FixedPrice/BuyAllowlist.t.sol
@@ -3,13 +3,22 @@ pragma solidity 0.8.20;
 
 import "test/minters/FixedPrice/FixedPriceTest.t.sol";
 
-contract BuyTokens is FixedPriceTest {
-    uint256 internal claimIndex;
+contract BuyAllowlist is FixedPriceTest, StandardMerkleTree {
+    uint256[] internal claimIndexes;
+    bytes32[] internal merkleTree;
+    bytes32[][] internal aliceProofs;
 
     function setUp() public override {
-        quantity = 1;
-        mintPassSignerPk = 1;
-        mintPassSigner = vm.addr(mintPassSignerPk);
+        _createAccounts();
+        address[5] memory users = [alice, bob, eve, susan, alice];
+        for (uint256 i; i < users.length; ++i) {
+            merkleTree.push(keccak256(bytes.concat(keccak256(abi.encode(i + 1, users[i])))));
+        }
+        merkleRoot = getRoot(merkleTree);
+
+        aliceProofs.push(getProof(merkleTree, 0));
+        assertEq(aliceProofs.length, 1);
+        claimIndexes.push(1);
         super.setUp();
     }
 
@@ -18,72 +27,30 @@ contract BuyTokens is FixedPriceTest {
         fixedPrice.buy{value: price}(fxGenArtProxy, mintId, quantity, alice);
     }
 
-    function test_BuyWithMintPass() public {
-        bytes32 digest = fixedPrice.generateTypedDataHash(claimIndex, alice);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(mintPassSignerPk, digest);
+    function test_BuyAllowlist() public {
         vm.prank(alice);
-        fixedPrice.buyMintPass{value: quantity * price}(
-            fxGenArtProxy,
-            mintId,
-            quantity,
-            alice,
-            claimIndex,
-            abi.encode(v, r, s)
-        );
+        fixedPrice.buyAllowlist{value: quantity * price}(fxGenArtProxy, mintId, claimIndexes, alice, aliceProofs);
     }
 
-    function test_RevertsWhen_NotClaimer_BuyWithMintPass() public {
-        bytes32 digest = fixedPrice.generateTypedDataHash(claimIndex, alice);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(mintPassSignerPk, digest);
+    function test_RevertsWhen_NotClaimer_BuyAllowlist() public {
         vm.prank(bob);
         vm.expectRevert();
-        fixedPrice.buyMintPass{value: quantity * price}(
-            fxGenArtProxy,
-            mintId,
-            quantity,
-            alice,
-            claimIndex,
-            abi.encode(v, r, s)
-        );
+        fixedPrice.buyAllowlist{value: quantity * price}(fxGenArtProxy, mintId, claimIndexes, alice, aliceProofs);
     }
 
-    function test_RevertsWhen_SignatureInvalid_BuyWithMintPass() public {
-        bytes32 digest = fixedPrice.generateTypedDataHash(claimIndex, alice);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(2, digest);
+    function test_RevertsWhen_ProofsInvalid_BuyAllowlist() public {
+        aliceProofs[0].pop();
         vm.prank(alice);
         vm.expectRevert();
-        fixedPrice.buyMintPass{value: quantity * price}(
-            fxGenArtProxy,
-            mintId,
-            quantity,
-            alice,
-            claimIndex,
-            abi.encode(v, r, s)
-        );
+        fixedPrice.buyAllowlist{value: quantity * price}(fxGenArtProxy, mintId, claimIndexes, alice, aliceProofs);
     }
 
-    function test_RevertsWhen_MintPassAlreadyClaimed_BuyWithMintPass() public {
-        bytes32 digest = fixedPrice.generateTypedDataHash(claimIndex, alice);
-        (uint8 v, bytes32 r, bytes32 s) = vm.sign(mintPassSignerPk, digest);
+    function test_RevertsWhen_SlotAlreadyClaimed_BuyAllowlist() public {
         vm.prank(alice);
-        fixedPrice.buyMintPass{value: quantity * price}(
-            fxGenArtProxy,
-            mintId,
-            quantity,
-            alice,
-            claimIndex,
-            abi.encode(v, r, s)
-        );
+        fixedPrice.buyAllowlist{value: quantity * price}(fxGenArtProxy, mintId, claimIndexes, alice, aliceProofs);
 
         vm.prank(alice);
         vm.expectRevert();
-        fixedPrice.buyMintPass{value: quantity * price}(
-            fxGenArtProxy,
-            mintId,
-            quantity,
-            alice,
-            claimIndex,
-            abi.encode(v, r, s)
-        );
+        fixedPrice.buyAllowlist{value: quantity * price}(fxGenArtProxy, mintId, claimIndexes, alice, aliceProofs);
     }
 }
