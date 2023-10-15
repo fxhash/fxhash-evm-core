14763c68f93c37889598d406307a541310bac1d9
Sahil Mehta <samehta91@gmail.com>
2023-10-14 17:12:14 -0700

    Clean up ticket redeemer contract and interface

--- src/interfaces/ITicketRedeemer.sol
+++ src/interfaces/ITicketRedeemer.sol
@@ -5,18 +5,23 @@ import {IMinter} from "src/interfaces/IMinter.sol";
 
 /**
  * @title ITicketRedeemer
- * @notice Minter contract for redeeming mint tickets to mint a FxGenArt721 token
+ * @author fxhash
+ * @notice Minter for redeeming FxGenArt721 tokens by burning FxMintTicket721 tokens
  */
 interface ITicketRedeemer is IMinter {
+    /*//////////////////////////////////////////////////////////////////////////
+                                  EVENTS
+    //////////////////////////////////////////////////////////////////////////*/
+
     /**
-     * @dev Emitted when the mint details are set for a ticket contract
-     * @param _ticket The address of the ticket contract
-     * @param _token The address of the token that can be minted by the ticket contract
+     * @notice Event emitted when the mint details are set for a ticket contract
+     * @param _ticket Address of the ticket contract
+     * @param _token Address of the token that can be minted by the ticket contract
      */
     event MintDetailsSet(address indexed _ticket, address indexed _token);
 
     /**
-     * @dev Emitted when a ticket is redeemed and a new token is minted
+     * @notice Event emitted when a ticket is redeemed and a new token is minted
      * @param _ticket The address of the ticket contract
      * @param _tokenId The ID of the ticket token that is burned
      * @param _owner The address of the owner of the new token
@@ -24,30 +29,39 @@ interface ITicketRedeemer is IMinter {
      */
     event Redeemed(address indexed _ticket, uint256 indexed _tokenId, address indexed _owner, address _token);
 
+    /*//////////////////////////////////////////////////////////////////////////
+                                  ERRORS
+    //////////////////////////////////////////////////////////////////////////*/
+
     /**
-     * @dev Throws an error indicating that the mint details are already set for a ticket contract
+     * @notice Error thrown when mint details are already set for a ticket contract
      */
     error AlreadySet();
+
     /**
-     * @dev Throws an error indicating that the token is invalid
+     * @notice Error thrown when token address is invalid
      */
     error InvalidToken();
+
     /**
-     * @dev Throws an error indicating that the caller is not authorized
+     * @notice Error thrown when the caller is not authorized
      */
     error NotAuthorized();
 
+    /*//////////////////////////////////////////////////////////////////////////
+                                  FUNCTIONS
+    //////////////////////////////////////////////////////////////////////////*/
+
     /**
      * @notice Burns a ticket and mints a new token to the caller
-     * @param _ticket The address of the ticket contract
-     * @param _tokenId The ID of the ticket token to burn
-     * @param _fxParams Random sequence of fixed-length bytes used as input
+     * @param _ticket Address of the ticket contract
+     * @param _tokenId ID of the ticket token to burn
+     * @param _fxParams Random sequence of fixed-length bytes used for token input
      */
     function redeem(address _ticket, uint256 _tokenId, bytes calldata _fxParams) external;
 
     /**
-     * @notice A mapping of tickets to the tokens they can mint
-     * @param _ticket The address of the ticket contract
+     * @notice Mapping of FxGenArt721 token address to FxMintTicket721 token address
      */
-    function tokens(address _ticket) external view returns (address);
+    function tokens(address) external view returns (address);
 }
--- src/minters/TicketRedeemer.sol
+++ src/minters/TicketRedeemer.sol
@@ -2,28 +2,39 @@
 pragma solidity 0.8.20;
 
 import {FxMintTicket721} from "src/tokens/FxMintTicket721.sol";
+
 import {IFxGenArt721, ReserveInfo} from "src/interfaces/IFxGenArt721.sol";
 import {IMinter} from "src/interfaces/IMinter.sol";
 import {ITicketRedeemer} from "src/interfaces/ITicketRedeemer.sol";
 
 /**
  * @title TicketRedeemer
+ * @author fxhash
  * @notice See the documentation in {ITicketRedeemer}
  */
 contract TicketRedeemer is ITicketRedeemer {
-    /// @inheritdoc ITicketRedeemer
+    /**
+     * @inheritdoc ITicketRedeemer
+     */
     mapping(address => address) public tokens;
 
-    /// @inheritdoc IMinter
+    /**
+     * @inheritdoc IMinter
+     */
     function setMintDetails(ReserveInfo calldata, bytes calldata _mintData) external {
+        // Decodes ticket address from mint data
         address ticket = abi.decode(_mintData, (address));
+        // Reverts if ticket address has alread been set
         if (tokens[ticket] != address(0)) revert AlreadySet();
         tokens[ticket] = msg.sender;
 
+        // Emits event when mint details have been set
         emit MintDetailsSet(ticket, msg.sender);
     }
 
-    /// @inheritdoc ITicketRedeemer
+    /**
+     * @inheritdoc ITicketRedeemer
+     */
     function redeem(address _ticket, uint256 _tokenId, bytes calldata _fxParams) external {
         // Reverts if caller is not owner of ticket
         address owner = FxMintTicket721(_ticket).ownerOf(_tokenId);
