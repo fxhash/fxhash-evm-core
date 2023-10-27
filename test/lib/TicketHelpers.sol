3c5804db0475b4e30ab888b14a37b5b7fc5e640a
Sahil Mehta <samehta91@gmail.com>
2023-10-26 21:35:36 -0700

    chore: move test helpers to lib directory

---
+++ test/lib/TicketHelpers.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+
+import {FxMintTicket721} from "src/tokens/FxMintTicket721.sol";
+import {IFxMintTicket721, MintInfo} from "src/interfaces/IFxMintTicket721.sol";
+import {ITicketRedeemer} from "src/interfaces/ITicketRedeemer.sol";
+import {MockMinter} from "test/mocks/MockMinter.sol";
+
+contract TicketHelpers is Test {
+    modifier prank(address _caller) {
+        vm.startPrank(_caller);
+        _;
+        vm.stopPrank();
+    }
+
+    function _claim(
+        address _claimer,
+        address _proxy,
+        uint256 _tokenId,
+        uint80 _newPrice,
+        uint256 _payment
+    ) internal prank(_claimer) {
+        IFxMintTicket721(_proxy).claim{value: _payment}(_tokenId, _newPrice);
+    }
+
+    function _deposit(
+        address _depositer,
+        address _proxy,
+        uint256 _tokenId,
+        uint256 _amount
+    ) internal prank(_depositer) {
+        IFxMintTicket721(_proxy).deposit{value: _amount}(_tokenId);
+    }
+
+    function _isMinter(address _proxy, address _minter) internal view returns (bool) {
+        return IFxMintTicket721(_proxy).minters(_minter);
+    }
+
+    function _mint(
+        address _caller,
+        address _minter,
+        address _proxy,
+        address _to,
+        uint256 _amount,
+        uint256 _payment
+    ) internal prank(_caller) {
+        MockMinter(_minter).mint(_proxy, _to, _amount, _payment);
+    }
+
+    function _redeem(
+        address _owner,
+        address _redeemer,
+        address _ticket,
+        uint256 _tokenId,
+        bytes storage _fxParams
+    ) internal prank(_owner) {
+        ITicketRedeemer(_redeemer).redeem(_ticket, _tokenId, _fxParams);
+    }
+
+    function _registerMinters(address _creator, address _proxy, MintInfo[] memory _mintInfo) internal prank(_creator) {
+        IFxMintTicket721(_proxy).registerMinters(_mintInfo);
+    }
+
+    function _setPrice(address _owner, address _proxy, uint256 _tokenId, uint80 _newPrice) internal prank(_owner) {
+        IFxMintTicket721(_proxy).setPrice(_tokenId, _newPrice);
+    }
+
+    function _setApprovalForAll(
+        address _owner,
+        address _proxy,
+        address _operator,
+        bool _approval
+    ) internal prank(_owner) {
+        FxMintTicket721(_proxy).setApprovalForAll(_operator, _approval);
+    }
+
+    function _transferFrom(
+        address _sender,
+        address _proxy,
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) internal prank(_sender) {
+        FxMintTicket721(_proxy).transferFrom(_from, _to, _tokenId);
+    }
+
+    function _withdraw(address _caller, address _proxy, address _to) internal prank(_caller) {
+        IFxMintTicket721(_proxy).withdraw(_to);
+    }
+}
---
+++ test/lib/TokenHelpers.sol
@@ -0,0 +1,96 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+
+import {IFxGenArt721, MintInfo} from "src/interfaces/IFxGenArt721.sol";
+import {ISeedConsumer} from "src/interfaces/ISeedConsumer.sol";
+import {MockMinter} from "test/mocks/MockMinter.sol";
+
+contract TokenHelpers is Test {
+    modifier prank(address _caller) {
+        vm.startPrank(_caller);
+        _;
+        vm.stopPrank();
+    }
+
+    function _burn(address _owner, address _proxy, uint256 _tokenId) internal prank(_owner) {
+        IFxGenArt721(_proxy).burn(_tokenId);
+    }
+
+    function _fulfillSeedRequest(
+        address _caller,
+        address _proxy,
+        uint256 _tokenId,
+        bytes32 _seed
+    ) internal prank(_caller) {
+        ISeedConsumer(_proxy).fulfillSeedRequest(_tokenId, _seed);
+    }
+
+    function _mint(address _minter, address _proxy, address _to, uint256 _amount, uint256 _payment) internal {
+        MockMinter(_minter).mint(_proxy, _to, _amount, _payment);
+    }
+
+    function _ownerMint(address _creator, address _proxy, address _to) internal prank(_creator) {
+        IFxGenArt721(_proxy).ownerMint(_to);
+    }
+
+    function _pause(address _admin, address _proxy) internal prank(_admin) {
+        IFxGenArt721(_proxy).pause();
+    }
+
+    function _reduceSupply(address _creator, address _proxy, uint120 _supply) internal prank(_creator) {
+        IFxGenArt721(_proxy).reduceSupply(_supply);
+    }
+
+    function _registerMinters(address _creator, address _proxy, MintInfo[] memory _mintInfo) internal prank(_creator) {
+        IFxGenArt721(_proxy).registerMinters(_mintInfo);
+    }
+
+    function _setBaseURI(
+        address _admin,
+        address _proxy,
+        string memory _uri,
+        bytes memory _signature
+    ) internal prank(_admin) {
+        IFxGenArt721(_proxy).setBaseURI(_uri, _signature);
+    }
+
+    function _setContractURI(
+        address _admin,
+        address _proxy,
+        string memory _uri,
+        bytes memory _signature
+    ) internal prank(_admin) {
+        IFxGenArt721(_proxy).setContractURI(_uri, _signature);
+    }
+
+    function _setImageURI(
+        address _admin,
+        address _proxy,
+        string memory _uri,
+        bytes memory _signature
+    ) internal prank(_admin) {
+        IFxGenArt721(_proxy).setImageURI(_uri, _signature);
+    }
+
+    function _setRandomizer(address _admin, address _proxy, address _randomizer) internal prank(_admin) {
+        IFxGenArt721(_proxy).setRandomizer(_randomizer);
+    }
+
+    function _setRenderer(address _admin, address _proxy, address _renderer) internal prank(_admin) {
+        IFxGenArt721(_proxy).setRenderer(_renderer);
+    }
+
+    function _toggleBurn(address _creator, address _proxy) internal prank(_creator) {
+        IFxGenArt721(_proxy).toggleBurn();
+    }
+
+    function _toggleMint(address _creator, address _proxy) internal prank(_creator) {
+        IFxGenArt721(_proxy).toggleMint();
+    }
+
+    function _unpause(address _admin, address _proxy) internal prank(_admin) {
+        IFxGenArt721(_proxy).unpause();
+    }
+}
