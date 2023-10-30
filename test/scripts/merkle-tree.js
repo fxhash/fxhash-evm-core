4b33511fb90259e1c79094b74547a432998d0575
steven nevins <steven.nevins.js@gmail.com>
2023-10-29 21:43:22 -0400

    feat(merkletree-cli): implementation to stdout

--- test/scripts/merkle-tree.js
+++ test/scripts/merkle-tree.js
@@ -6,39 +6,38 @@ program
 	.description("CLI to Create MerkleTrees and Get Proofs");
 
 program
-	.command("tree")
+	.command("root")
 	.description("Generate a merkle tree from an array of unhashed values")
 	.argument("<string>", "array of unhashed leaves")
 	.option("-e, --encoding <string>", "encoding of unhashed leaves")
 	.action((array, options) => {
-		console.log(array);
-	});
-
-program
-	.command("root")
-	.description("Returns the root of merkle tree")
-	.argument("<string>", "array of the elements of a merkle tree")
-	.action((array) => {
-		console.log(array);
+		const values = JSON.parse(array);
+		const tree = StandardMerkleTree.of(values, JSON.parse(options.encoding));
+		process.stdout.write(tree.root);
 	});
 
 program
 	.command("proof")
-	.description("Returns the root of merkle tree")
-	.argument("<string>", "array of the elements of a merkle tree")
-
-	.option("-l, --leaf <string>", "index of the leaf to prove")
+	.description("Returns the proofs for an element of a merkle tree")
+	.argument("<string>", "array of unhashed leaves")
+	.option("-e, --encoding <string>", "encoding of unhashed leaves")
+	.option("-i, --index <string>", "index of the leaf to prove")
 	.action((array, options) => {
-		console.log(array);
+		const values = JSON.parse(array);
+		const tree = StandardMerkleTree.of(values, JSON.parse(options.encoding));
+		process.stdout.write(tree.getProof(options.index));
 	});
 
 program
 	.command("multiProof")
 	.description("Returns the root of merkle tree")
 	.argument("<string>", "array of the elements of a merkle tree")
-
-	.option("-l, --leaves <string>", "indexes of leaves to prove")
+	.option("-e, --encoding <string>", "encoding of unhashed leaves")
+	.option("-i, --indexes <string>", "array of indexes of leaves to prove")
 	.action((array, options) => {
-		console.log(array);
+		const values = JSON.parse(array);
+		const tree = StandardMerkleTree.of(values, JSON.parse(options.encoding));
+		process.stdout.write(tree.getMultiProof(JSON.parse(options.indexes)));
 	});
+
 program.parse();
