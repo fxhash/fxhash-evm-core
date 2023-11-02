cf35c7075a4470d680ebba12702fddd86a3cc277
steven nevins <steven.nevins.js@gmail.com>
2023-11-02 09:56:07 -0400

    test: add invalid redeemer test

--- test/factories/FxTicketFactory/FxTicketFactoryTest.t.sol
+++ test/factories/FxTicketFactory/FxTicketFactoryTest.t.sol
@@ -8,6 +8,7 @@ contract FxTicketFactoryTest is BaseTest {
     bytes4 INVALID_GRACE_PERIOD_ERROR = IFxTicketFactory.InvalidGracePeriod.selector;
     bytes4 INVALID_OWNER_ERROR = IFxTicketFactory.InvalidOwner.selector;
     bytes4 INVALID_TOKEN_ERROR = IFxTicketFactory.InvalidToken.selector;
+    bytes4 INVALID_REDEEMER_ERROR = IFxTicketFactory.InvalidRedeemer.selector;
 
     /*//////////////////////////////////////////////////////////////////////////
                                      SETUP
