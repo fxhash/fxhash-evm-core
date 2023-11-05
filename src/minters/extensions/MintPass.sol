ff9ace49cfe92747ec31bfd649d236f1b1f5e48d
steven nevins <steven.nevins.js@gmail.com>
2023-11-05 09:48:44 -0500

    feat: add a reserve nonce to mint pass signatures

--- src/minters/extensions/MintPass.sol
+++ src/minters/extensions/MintPass.sol
@@ -21,7 +21,10 @@ abstract contract MintPass is EIP712 {
      */
     mapping(address => mapping(uint256 => address)) public signingAuthorities;
 
-    mapping(address => mapping(uint256 => uint256)) public signerNonces;
+    /**
+     * @notice Mapping of token address to reserve ID to reserve nonce
+     */
+    mapping(address => mapping(uint256 => uint256)) public reserveNonce;
 
     /*//////////////////////////////////////////////////////////////////////////
                                     EVENTS
@@ -74,11 +77,11 @@ abstract contract MintPass is EIP712 {
     function generateTypedDataHash(
         address _token,
         uint256 _reserveId,
-        uint256 _signerNonce,
+        uint256 _reserveNonce,
         uint256 _index,
         address _claimer
     ) public view returns (bytes32) {
-        bytes32 structHash = keccak256(abi.encode(CLAIM_TYPEHASH, _token, _signerNonce, _reserveId, _index, _claimer));
+        bytes32 structHash = keccak256(abi.encode(CLAIM_TYPEHASH, _token, _reserveNonce, _reserveId, _index, _claimer));
         return _hashTypedDataV4(structHash);
     }
 
@@ -102,7 +105,7 @@ abstract contract MintPass is EIP712 {
         BitMaps.BitMap storage _bitmap
     ) internal {
         if (_bitmap.get(_index)) revert PassAlreadyClaimed();
-        uint256 nonce = signerNonces[_token][_reserveId];
+        uint256 nonce = reserveNonce[_token][_reserveId];
         bytes32 hash = generateTypedDataHash(_token, _reserveId, nonce, _index, msg.sender);
         address signer = signingAuthorities[_token][_reserveId];
         if (!signer.isValidSignatureNow(hash, _signature)) revert InvalidSignature();
