42b449db39a6f72cdc7f7507ca9e667654d0c60c
steven nevins <steven.nevins.js@gmail.com>
2023-11-05 19:22:08 -0500

    docs: remove ascii text

--- src/tokens/FxGenArt721.sol
+++ src/tokens/FxGenArt721.sol
@@ -1,19 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-/*######################################################################
-#                                                                      #
-#                        .#                                    .#      #
-#      .##             .## .##                         .##      .##    #
-#    .#               .##  .##                         .##       .##   #
-#  .###### .##   .## .##   .#####.     .####.   .####  .#####.    .##  #
-#   .##     .# .##  .##   .##  .##  .##  .##  .##     .##  .##   .##   #
-#   .##      .#     .##   .##  .## .##   .##    .###  .##  .##   .##   #
-#   .##    .#  .##   .##  .##  .## .##   ,##      .## .#   .##  .##    #
-#   .##   .##   .##   .## .##  .##   .##  ,#  .## .## .##  .## .##     #
-#                       .#                                    .#       #
-######################################################################*/
-
 import {EIP712} from "openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ERC721} from "openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Initializable} from "openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
