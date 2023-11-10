fe94ba49d9b09b9929983ed1b6cd3d06f3d76102
steven nevins <steven.nevins.js@gmail.com>
2023-11-10 11:08:18 -0500

    feat: add events for contract, base, and image URI updates

--- src/tokens/FxGenArt721.sol
+++ src/tokens/FxGenArt721.sol
@@ -276,6 +276,7 @@ contract FxGenArt721 is IFxGenArt721, IERC4906, ERC721, EIP712, Initializable, O
         bytes32 digest = generateTypedDataHash(SET_BASE_URI_TYPEHASH, _uri);
         _verifySignature(digest, _signature);
         metadataInfo.baseURI = _uri;
+        emit SetBaseURI(_uri);
         emit BatchMetadataUpdate(1, totalSupply);
     }
 
@@ -286,6 +287,7 @@ contract FxGenArt721 is IFxGenArt721, IERC4906, ERC721, EIP712, Initializable, O
         bytes32 digest = generateTypedDataHash(SET_CONTRACT_URI_TYPEHASH, _uri);
         _verifySignature(digest, _signature);
         issuerInfo.projectInfo.contractURI = _uri;
+        emit SetContractURI(_uri);
     }
 
     /**
@@ -295,6 +297,7 @@ contract FxGenArt721 is IFxGenArt721, IERC4906, ERC721, EIP712, Initializable, O
         bytes32 digest = generateTypedDataHash(SET_IMAGE_URI_TYPEHASH, _uri);
         _verifySignature(digest, _signature);
         metadataInfo.imageURI = _uri;
+        emit SetImageURI(_uri);
     }
 
     /**
