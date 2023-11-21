acee0fa775dbd96be0b8ac977dc75aee013d5e41
steven nevins <steven.nevins.js@gmail.com>
2023-11-21 10:57:25 -0500

    chore: use helpers

--- test/factories/FxIssuerFactory/GetTokenAddress.t.sol
+++ test/factories/FxIssuerFactory/GetTokenAddress.t.sol
@@ -6,9 +6,7 @@ import "test/factories/FxIssuerFactory/FxIssuerFactoryTest.t.sol";
 contract GetTokenAddress is FxIssuerFactoryTest {
     function test_GetTokenAddress() public {
         address deterministicAddr = fxIssuerFactory.getTokenAddress(address(this));
-        fxGenArtProxy = fxIssuerFactory.createProject(
-            abi.encode(creator, initInfo, projectInfo, metadataInfo, mintInfo, royaltyReceivers, basisPoints)
-        );
+        _createProject();
         assertEq(fxGenArtProxy, deterministicAddr);
     }
 }
--- test/factories/FxTicketFactory/GetTicketAddress.t.sol
+++ test/factories/FxTicketFactory/GetTicketAddress.t.sol
@@ -6,16 +6,7 @@ import "test/factories/FxTicketFactory/FxTicketFactoryTest.t.sol";
 contract GetTicketAddress is FxTicketFactoryTest {
     function test_GetTicketAddress() public {
         address deterministicAddr = fxTicketFactory.getTicketAddress(address(this));
-        fxMintTicketProxy = fxTicketFactory.createTicket(
-            abi.encode(
-                creator,
-                fxGenArtProxy,
-                address(ticketRedeemer),
-                address(ipfsRenderer),
-                uint48(ONE_DAY),
-                mintInfo
-            )
-        );
+        _createTicket();
         assertEq(fxMintTicketProxy, deterministicAddr);
     }
 }
