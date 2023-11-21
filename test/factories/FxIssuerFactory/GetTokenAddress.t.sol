29a5cf22886512801494b57ff95bad5b15eba526
steven nevins <steven.nevins.js@gmail.com>
2023-11-21 10:40:28 -0500

    fix: merge conflicts

--- test/factories/FxIssuerFactory/GetTokenAddress.t.sol
+++ test/factories/FxIssuerFactory/GetTokenAddress.t.sol
@@ -4,20 +4,10 @@ pragma solidity 0.8.23;
 import "test/factories/FxIssuerFactory/FxIssuerFactoryTest.t.sol";
 
 contract GetTokenAddress is FxIssuerFactoryTest {
-    function setUp() public virtual override {
-        super.setUp();
-    }
-
     function test_GetTokenAddress() public {
         address deterministicAddr = fxIssuerFactory.getTokenAddress(address(this));
         fxGenArtProxy = fxIssuerFactory.createProject(
-            creator,
-            initInfo,
-            projectInfo,
-            metadataInfo,
-            mintInfo,
-            royaltyReceivers,
-            basisPoints
+            abi.encode(creator, initInfo, projectInfo, metadataInfo, mintInfo, royaltyReceivers, basisPoints)
         );
         assertEq(fxGenArtProxy, deterministicAddr);
     }
--- test/factories/FxTicketFactory/GetTicketAddress.t.sol
+++ test/factories/FxTicketFactory/GetTicketAddress.t.sol
@@ -4,19 +4,17 @@ pragma solidity 0.8.23;
 import "test/factories/FxTicketFactory/FxTicketFactoryTest.t.sol";
 
 contract GetTicketAddress is FxTicketFactoryTest {
-    function setUp() public virtual override {
-        super.setUp();
-    }
-
     function test_GetTicketAddress() public {
         address deterministicAddr = fxTicketFactory.getTicketAddress(address(this));
         fxMintTicketProxy = fxTicketFactory.createTicket(
-            creator,
-            fxGenArtProxy,
-            address(ticketRedeemer),
-            address(ipfsRenderer),
-            uint48(ONE_DAY),
-            mintInfo
+            abi.encode(
+                creator,
+                fxGenArtProxy,
+                address(ticketRedeemer),
+                address(ipfsRenderer),
+                uint48(ONE_DAY),
+                mintInfo
+            )
         );
         assertEq(fxMintTicketProxy, deterministicAddr);
     }
