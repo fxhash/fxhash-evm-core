a6d979612dfbd86a4d30025a42ec420070e0c07f
steven nevins <steven.nevins.js@gmail.com>
2023-11-28 12:29:03 -0500

    fix: inheritdoc in mint ticket and use IFxGenArt721

--- src/tokens/FxMintTicket721.sol
+++ src/tokens/FxMintTicket721.sol
@@ -14,7 +14,6 @@ import {IAccessControl} from "openzeppelin/contracts/access/IAccessControl.sol";
 import {IERC4906} from "openzeppelin/contracts/interfaces/IERC4906.sol";
 import {IFxContractRegistry} from "src/interfaces/IFxContractRegistry.sol";
 import {IFxGenArt721, MintInfo, ProjectInfo, ReserveInfo} from "src/interfaces/IFxGenArt721.sol";
-import {IToken} from "src/interfaces/IToken.sol";
 import {IFxMintTicket721, TaxInfo} from "src/interfaces/IFxMintTicket721.sol";
 import {IMinter} from "src/interfaces/IMinter.sol";
 import {IRenderer} from "src/interfaces/IRenderer.sol";
@@ -414,10 +413,10 @@ contract FxMintTicket721 is IFxMintTicket721, IERC4906, ERC721, Initializable, O
     }
 
     /**
-     * @inheritdoc IToken
+     * @inheritdoc IFxMintTicket721
      */
     function primaryReceiver() external view returns (address) {
-        return IToken(genArt721).primaryReceiver();
+        return IFxGenArt721(genArt721).primaryReceiver();
     }
 
     /**
