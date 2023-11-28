8beb80b51fedb61982696ee68d87617cbfe4ca88
Sahil Mehta <samehta91@gmail.com>
2023-11-28 19:23:27 +0100

    fix: rename var

--- src/minters/DutchAuction.sol
+++ src/minters/DutchAuction.sol
@@ -287,8 +287,8 @@ contract DutchAuction is IDutchAuction, Allowlist, MintPass {
         if (msg.value != price * _amount) revert InvalidPayment();
 
         // Updates the allocation for the reserve
-        uint128 packed_amount = SafeCastLib.safeCastTo128(_amount);
-        reserve.allocation -= packed_amount;
+        uint128 amount = SafeCastLib.safeCastTo128(_amount);
+        reserve.allocation -= amount;
 
         // If the reserve allocation is fully sold out and refunds are enabled, store the last price
         if (reserve.allocation == 0 && daInfo.refunded) {
@@ -298,7 +298,7 @@ contract DutchAuction is IDutchAuction, Allowlist, MintPass {
         // Updates the minter's total mints and total paid amounts
         uint128 totalPayment = SafeCastLib.safeCastTo128(price * _amount);
         MinterInfo storage minterInfo = refunds[_token][_reserveId].minterInfo[msg.sender];
-        minterInfo.totalMints += packed_amount;
+        minterInfo.totalMints += amount;
         minterInfo.totalPaid += totalPayment;
 
         // Adds the sale proceeds to the total for the reserve
