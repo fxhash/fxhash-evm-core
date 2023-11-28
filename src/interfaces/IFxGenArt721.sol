2669a37244f9ba67b0a861a0037e62b950b915ec
steven nevins <steven.nevins.js@gmail.com>
2023-11-28 13:38:29 -0500

    chore: primary receiver

--- src/interfaces/IFxGenArt721.sol
+++ src/interfaces/IFxGenArt721.sol
@@ -54,11 +54,11 @@ interface IFxGenArt721 is ISeedConsumer, IToken {
 
     /**
      * @notice Event emitted when the primary receiver address is updated
-     * @param _receiver The split address receiving funds on behalf of the users
+     * @param _primaryReceiver The split address receiving funds on behalf of the users
      * @param _receivers Array of addresses receiving a portion of the funds in a split
      * @param _allocations Array of allocation shares for the split
      */
-    event PrimaryReceiverUpdated(address indexed _receiver, address[] _receivers, uint32[] _allocations);
+    event PrimaryReceiverUpdated(address indexed _primaryReceiver, address[] _receivers, uint32[] _allocations);
 
     /**
      * @notice Event emitted when project tags are set
