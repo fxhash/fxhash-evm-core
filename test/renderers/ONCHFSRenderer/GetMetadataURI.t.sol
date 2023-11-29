a232ff05d7b69668fd2422768d11414c1eb036ae
swaHili <samehta91@gmail.com>
2023-11-28 18:16:22 -0800

    feat: implement ONCHFSRenderer for onchain metadata (#270)
    * feat: implement onchfs renderer
    
    * fix: natspec

--- test/renderers/ONCHFSRenderer/GetMetadataURI.t.sol
+++ test/renderers/ONCHFSRenderer/GetMetadataURI.t.sol
@@ -1,22 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import "test/renderers/IPFSRenderer/IPFSRendererTest.t.sol";
+import "test/renderers/ONCHFSRenderer/ONCHFSRendererTest.t.sol";
 
-contract GetMetadataURI is IPFSRendererTest {
+contract GetMetadataURI is ONCHFSRendererTest {
     using Strings for uint160;
     using Strings for uint256;
 
     function test_GetMetadataURI_DefaultURI() public {
         contractAddr = uint160(deployer).toHexString(20);
         generatedURI = string.concat(defaultMetadataURI, contractAddr, "/", tokenId.toString(), METADATA_ENDPOINT);
-        metadataURI = ipfsRenderer.getMetadataURI(deployer, "", tokenId);
+        metadataURI = onchfsRenderer.getMetadataURI(deployer, "", tokenId);
         assertEq(generatedURI, metadataURI);
     }
 
     function test_GetMetadataURI_BaseURI() public {
-        generatedURI = string.concat(string(IPFS_BASE_URI), "/", tokenId.toString(), METADATA_ENDPOINT);
-        metadataURI = ipfsRenderer.getMetadataURI(deployer, string(IPFS_BASE_URI), tokenId);
+        generatedURI = string.concat(string(ONCHFS_BASE_URI), "/", tokenId.toString(), METADATA_ENDPOINT);
+        metadataURI = onchfsRenderer.getMetadataURI(deployer, string(ONCHFS_BASE_URI), tokenId);
         assertEq(generatedURI, metadataURI);
     }
 }
--- test/renderers/ONCHFSRenderer/TokenURI.t.sol
+++ test/renderers/ONCHFSRenderer/TokenURI.t.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import "test/renderers/IPFSRenderer/IPFSRendererTest.t.sol";
+import "test/renderers/ONCHFSRenderer/ONCHFSRendererTest.t.sol";
 
-contract TokenURI is IPFSRendererTest {
+contract TokenURI is ONCHFSRendererTest {
     using Strings for uint160;
     using Strings for uint256;
 
@@ -12,14 +12,14 @@ contract TokenURI is IPFSRendererTest {
         data = abi.encode(metadataInfo.baseURI, metadataInfo.onchainPointer, genArtInfo.seed, genArtInfo.fxParams);
         contractAddr = uint160(deployer).toHexString(20);
         generatedURI = string.concat(defaultMetadataURI, contractAddr, "/", tokenId.toString(), METADATA_ENDPOINT);
-        tokenURI = ipfsRenderer.tokenURI(tokenId, data);
+        tokenURI = onchfsRenderer.tokenURI(tokenId, data);
         assertEq(generatedURI, tokenURI);
     }
 
     function test_TokenURI_BaseURI() public {
         data = abi.encode(metadataInfo.baseURI, metadataInfo.onchainPointer, genArtInfo.seed, genArtInfo.fxParams);
-        generatedURI = string.concat(string(IPFS_BASE_URI), "/", tokenId.toString(), METADATA_ENDPOINT);
-        tokenURI = ipfsRenderer.tokenURI(tokenId, data);
+        generatedURI = string.concat(string(ONCHFS_BASE_URI), "/", tokenId.toString(), METADATA_ENDPOINT);
+        tokenURI = onchfsRenderer.tokenURI(tokenId, data);
         assertEq(generatedURI, tokenURI);
     }
 }
