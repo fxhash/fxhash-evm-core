d6fd3e7592b1ad0371d2877131601a5d5ad1c2dd
Sahil Mehta <samehta91@gmail.com>
2024-04-15 12:27:26 -0700

    fix: remove to fix merge conflicts

--- test/minters/SignatureFrame/Mint.t.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
-
-import "test/minters/SignatureFrame/SignatureFrameTest.t.sol";
-
-contract Mint is SignatureFrameTest {
-    function test_Mint() public {}
-}
