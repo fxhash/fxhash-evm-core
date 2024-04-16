49e51222ac9a121c18ad7143d9ca5653eb0938f4
Sahil Mehta <samehta91@gmail.com>
2024-04-16 10:44:50 -0700

    fix: refactor

--- test/minters/FarcasterFrame/Ownable.t.sol
+++ test/minters/FarcasterFrame/Ownable.t.sol
@@ -4,13 +4,6 @@ pragma solidity 0.8.23;
 import "test/minters/FarcasterFrame/FarcasterFrameTest.t.sol";
 
 contract Ownable is FarcasterFrameTest {
-    address internal newController;
-
-    function setUp() public override {
-        super.setUp();
-        newController = address(20);
-    }
-
     function test_Pause() public {
         vm.prank(admin);
         farcasterFrame.pause();
@@ -20,7 +13,7 @@ contract Ownable is FarcasterFrameTest {
     function test_RevertsWhen_NotOwner_Pause() public {
         vm.expectRevert();
         farcasterFrame.pause();
-        assertTrue(!farcasterFrame.paused());
+        assertFalse(farcasterFrame.paused());
     }
 
     function test_Unpause() public {
@@ -28,7 +21,7 @@ contract Ownable is FarcasterFrameTest {
         farcasterFrame.pause();
         farcasterFrame.unpause();
         vm.stopPrank();
-        assertTrue(!farcasterFrame.paused());
+        assertFalse(farcasterFrame.paused());
     }
 
     function test_RevertsWhen_NotOwner_Unpause() public {
@@ -41,12 +34,12 @@ contract Ownable is FarcasterFrameTest {
 
     function test_SetController() public {
         vm.prank(admin);
-        farcasterFrame.setController(newController);
-        assertEq(farcasterFrame.controller(), newController);
+        farcasterFrame.setController(alice);
+        assertEq(farcasterFrame.controller(), alice);
     }
 
     function test_RevertsWhen_NotOwner_SetController() public {
         vm.expectRevert();
-        farcasterFrame.setController(newController);
+        farcasterFrame.setController(alice);
     }
 }
