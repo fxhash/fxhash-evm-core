6280b7d24b1054ebe08f3c8c802e2bca15adc719
Sahil Mehta <samehta91@gmail.com>
2024-04-16 10:48:21 -0700

    fix: clean up

--- script/VerifyArtist.s.sol
+++ script/VerifyArtist.s.sol
@@ -1,25 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {Script} from "forge-std/Script.sol";
-import {CREATOR_ROLE} from "../src/utils/Constants.sol";
-import {FxRoleRegistry} from "../src/registries/FxRoleRegistry.sol";
+import "forge-std/Script.sol";
+import "src/utils/Constants.sol";
+
+import {FxRoleRegistry} from "src/registries/FxRoleRegistry.sol";
 
 contract VerifyArtist is Script {
     address[] internal artists;
     address internal constant ROLE_REGISTRY = 0x8d3C748e99066e15425BA1620cdD066d85D6d918;
 
     function setUp() public {
-        artists.push(address(420));
+        artists.push();
     }
 
     function run() public {
         vm.startBroadcast();
-
         for (uint256 i; i < artists.length; i++) {
             FxRoleRegistry(ROLE_REGISTRY).grantRole(CREATOR_ROLE, artists[i]);
         }
-
         vm.stopBroadcast();
     }
 }
